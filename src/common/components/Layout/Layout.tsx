0cd2e10a78cd947f4dcec66f0f310d85646e915b
k2 <izmail.k2@gmail.com>
2024-08-24 15:01:46 +0300

    feat: conditionally render Sidebar based on user authentication in Layout

--- src/common/components/Layout/Layout.tsx
+++ src/common/components/Layout/Layout.tsx
@@ -3,11 +3,16 @@ import { PropsWithChildren, ReactElement } from 'react'
 import { NavBar, Sidebar } from '@/common/components'
 
 export const Layout: NextPage<PropsWithChildren> = ({ children }) => {
+  const authenticated = false //возможно тут можно выполнять запрос для получения данных о пользователе
+
+  //пока данные грузятся можно сделать индикатор загрузки
+
   return (
     <div className="flex min-h-screen flex-col">
       <NavBar /> {/* Верхняя навигационная панель */}
       <div className="flex flex-1 w-full">
-        <Sidebar /> {/* Sidebar слева */}
+        {authenticated && <Sidebar />}
+        {/* Sidebar отображается только если пользователь авторизован */}
         <main className="flex-1 p-10">
           {children} {/* Основной контент */}
         </main>
