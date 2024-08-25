8d1c2540cb24e7c446188791168fafc083ae562e
k2 <izmail.k2@gmail.com>
2024-08-25 22:07:02 +0300

    refactor: move NotFound component directly to 404 page with layout support

--- src/features/ui/auth/notFound/Notfound.tsx
+++
@@ -1,1 +0,0 @@
-export const NotFound = () => <div>404 Not Found!</div>
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,7 @@
-import { NotFound } from '@/features/ui/auth'
 import { getLayout } from '@/common/components'
+import { NextPageWithLayout } from '@/pages/_app'
+
+const NotFound: NextPageWithLayout = () => <div>404 Not Found!</div>
 
 NotFound.getLayout = getLayout
 export default NotFound
