2fd06e8029d614fef825459c77a235bad94005c0
k2 <izmail.k2@gmail.com>
2024-09-15 22:01:01 +0300

    feat: add instance API for the whole application

---
+++ src/appRoot/services/instaLeadersApi.ts
@@ -0,0 +1,8 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// Define a service using a base URL and expected endpoints
+export const instaLeadersApi = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://main.sociable-people.com/api' }),
+  endpoints: () => ({}),
+  reducerPath: 'instaLeadersApi',
+})
