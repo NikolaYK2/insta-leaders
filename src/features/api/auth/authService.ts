b84aad38229437417cfc70a6e3747b166d89e9b7
k2 <izmail.k2@gmail.com>
2024-09-15 22:03:06 +0300

    feat: split auth endpoints in authService - temp comment

---
+++ src/features/api/auth/authService.ts
@@ -0,0 +1,15 @@
+import { instaLeadersApi } from '@/appRoot/services/instaLeadersApi'
+
+const AUTH = 'v1/auth'
+const authService = instaLeadersApi.injectEndpoints({
+  endpoints: builder => ({
+    //Привер
+    // auth: builder.query<void, void>({
+    //   query: () => ({
+    //     url: `${AUTH}/hello`,
+    //   }),
+    // }),
+  }),
+})
+//пример
+export const { useAuthQuery } = authService
