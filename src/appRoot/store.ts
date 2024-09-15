388273ebec500fa7344bd1558f2a8facb521fbdf
k2 <izmail.k2@gmail.com>
2024-09-15 21:58:57 +0300

    feat: add store - store hooks in a separate file

---
+++ src/appRoot/store.ts
@@ -0,0 +1,17 @@
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit'
+import { createWrapper } from 'next-redux-wrapper'
+import { instaLeadersApi } from '@/appRoot/services/instaLeadersApi'
+
+const makeStore = () =>
+  configureStore({
+    reducer: { [instaLeadersApi.reducerPath]: instaLeadersApi.reducer },
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(instaLeadersApi.middleware),
+    devTools: true,
+  })
+
+export type AppStore = ReturnType<typeof makeStore>
+export type AppState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppState, unknown, Action>
+
+export const wrapper = createWrapper<AppStore>(makeStore)
