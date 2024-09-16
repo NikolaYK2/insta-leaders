183fcbe63a2e0dd0116af5ef866ee7faf2368a33
klrotex <klrotex11@gmail.com>
2024-09-16 22:45:45 +0300

    feat: add colors in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,53 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          100: '#73a5ff',
+          300: '#4c8dff',
+          500: '#397df6',
+          700: '#2f68cc',
+          900: '#234e99',
+        },
+        success: {
+          100: '#80ffbf',
+          300: '#22e584',
+          500: '#14cc70',
+          700: '#0f9954',
+          900: '#0a6638',
+        },
+        danger: {
+          100: '#ff8099',
+          300: '#f23d61',
+          500: '#cc1439',
+          700: '#990f2b',
+          900: '#660a1d',
+        },
+        warning: {
+          100: '#ffd073',
+          300: '#e5ac39',
+          500: '#d99000',
+          700: '#996600',
+          900: '#664400',
+        },
+        dark: {
+          100: '#4c4c4c',
+          300: '#333333',
+          500: '#171717',
+          700: '#0d0d0d',
+          900: '#000000',
+        },
+        light: {
+          100: '#ffffff',
+          300: '#f7fbff',
+          500: '#edf3fa',
+          700: '#d5dae0',
+          900: '#8d9094',
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        // 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        // 'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
