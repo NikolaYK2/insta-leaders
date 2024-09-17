156e8b3bd1dbae60bc26744c51a7c2401ef18cdf
k2 <izmail.k2@gmail.com>
2024-09-17 18:56:08 +0300

    feat: connect route TermsOfService

---
+++ src/pages/termsOfService/index.tsx
@@ -0,0 +1,5 @@
+import { TermsOfService } from '@/features/auth/ui/termsOfService/TermsOfService'
+import { getLayout } from '@/common/components'
+
+TermsOfService.getLayout = getLayout
+export default TermsOfService
