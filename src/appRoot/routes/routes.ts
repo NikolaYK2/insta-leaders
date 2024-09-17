5bd37a828db4990053151e5ad7bcfb6c3853cbd5
k2 <izmail.k2@gmail.com>
2024-09-17 18:28:16 +0300

    refactor: add routes - moved routes to constants

---
+++ src/appRoot/routes/routes.ts
@@ -0,0 +1,17 @@
+export const ROUTES_AUTH = {
+  REGISTRATION: '/signUp',
+  LOGIN: '/signIn',
+  FORGOT_PASSWORD: '/forgotPassword',
+  RECOVERY_PASSWORD: '/passwordRecovery',
+  CREATE_NEW_PASSWORD: '/createNewPassword',
+}
+
+export const ROUTES_APP = {
+  HOME: '/',
+  CREATE: '/create',
+  PROFILE: '/myProfile',
+  MESSENGER: '/messenger',
+  SEARCH: '/search',
+  STATISTICS: '/statistics',
+  FAVORITES: '/favorites',
+}
