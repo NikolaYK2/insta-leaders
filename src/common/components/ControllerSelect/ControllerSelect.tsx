48382405a9b152c07cc93ac951a5d084ee4f3197
k2 <izmail.k2@gmail.com>
2024-09-17 17:49:16 +0300

    feat: add ControllerSelect.tsx

--- src/common/components/ControllerSelect/ControllerSelect.tsx
+++ src/common/components/ControllerSelect/ControllerSelect.tsx
@@ -1,5 +1,21 @@
 import React from 'react'
+import { Control, FieldValues, useController, UseControllerProps } from 'react-hook-form'
+import { Selector } from '@nikolajk2/lib-insta-leaders'
+import { SelectProps } from '@radix-ui/react-select'
 
-export const ControllerSelect = () => {
-  return <div></div>
+type Props<T extends FieldValues> = Omit<UseControllerProps<T>, 'rules' | 'shouldUnregister'> &
+  SelectProps & {
+    control: Control<T>
+  }
+
+export const ControllerSelect = <T extends FieldValues>({
+  control,
+  name,
+  defaultValue,
+  ...props
+}: Props<T>) => {
+  const {
+    field: { value, name, onChange },
+  } = useController({ control, name, defaultValue })
+  return <Selector value={value} onValueChange={onChange} name={name} {...props} />
 }
