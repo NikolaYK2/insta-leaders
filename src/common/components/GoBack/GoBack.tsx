68014abfec3954f57ad70c2563b947abc3c2ecd2
k2 <izmail.k2@gmail.com>
2024-09-18 17:47:31 +0300

    refactor: upd style - add export

--- src/common/components/GoBack/GoBack.tsx
+++ src/common/components/GoBack/GoBack.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button, DynamicIcon, Typography, TypographyVariant } from '@nikolajk2/lib-insta-leaders'
 import Link from 'next/link'
+import { cn } from '@/common/utils/cn'
 
 type GoBackProps = {
   backToTitle?: string
@@ -8,16 +9,11 @@ type GoBackProps = {
 }
 export const GoBack = ({ backToRedirect, backToTitle }: GoBackProps) => {
   return (
-    <Button variant={'text'} asChild className={'text-amber-50'}>
+    <Button variant={'text'} asChild className={cn('!p-0 !text-light-100')}>
       <Link href={backToRedirect}>
-        <DynamicIcon
-          iconId={'ArrowBackOutline'}
-          width={24}
-          height={24}
-          className={'text-amber-50'}
-        />
-        <Typography variant={TypographyVariant.regular_text_14} className={'text-amber-50'}>
-          Back to {' ' + backToTitle}
+        <DynamicIcon iconId={'ArrowBackOutline'} width={24} height={24} />
+        <Typography variant={TypographyVariant.regular_text_14}>
+          Back to <span className={'capitalize'}>{backToTitle}</span>
         </Typography>
       </Link>
     </Button>
