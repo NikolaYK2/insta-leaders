bbb9004cdccac34b4470d051511489fe7c5b2d9a
k2 <izmail.k2@gmail.com>
2024-09-18 17:27:34 +0300

    feat: added logic for paragraph breaks - add style

--- src/common/components/LegalDocument/LegalDocument.tsx
+++ src/common/components/LegalDocument/LegalDocument.tsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { H1Title } from '@/common/components/H1Title/H1Title'
 import { Typography, TypographyVariant } from '@nikolajk2/lib-insta-leaders'
 
 type LegalDocumentProps = {
   title: string
-  description: string
+  description: string //для абзацов с красной строки нужно ставить перед ними символ \n
 }
 export const LegalDocument = ({ title, description }: LegalDocumentProps) => {
   return (
-    <div>
-      <H1Title title={title} />
+    <div className={'flex flex-col justify-center items-center w-full mt-[1.917%]'}>
+      <H1Title title={title} className={'mb-[1.667%]'} />
       <Typography
         variant={TypographyVariant.regular_text_14}
-        className={'text-center max-w-[958px]'}
+        className={'text-center max-w-[958px] '}
       >
-        {description}
+        {description.split('\n').map((line, i) => (
+          <Fragment key={i}>
+            {line}
+            <br />
+          </Fragment>
+        ))}
       </Typography>
     </div>
   )
