6c2d0d65e942b8fd1ff6547e95a1c9712fba6966
albogachiev <albogachiev-2014@mail.ru>
2024-09-25 17:33:00 +0300

    IL-74 fix: SignUp component

--- src/features/auth/api/authService.ts
+++ src/features/auth/api/authService.ts
@@ -43,7 +43,6 @@ const authService = instaLeadersApi.injectEndpoints({
       }),
     }),
   }),
-  overrideExisting: true,
 })
 //пример
 export const { useRegistrationMutation, useConfirmEmailMutation, useResendEmailMutation } =
