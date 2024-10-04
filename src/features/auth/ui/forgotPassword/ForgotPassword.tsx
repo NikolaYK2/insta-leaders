639b3b27e6739f2af4b1174026f7d7c23295c4da
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-10-04 21:46:35 +0400

    IL-72.3 final result

--- src/features/auth/ui/forgotPassword/ForgotPassword.tsx
+++ src/features/auth/ui/forgotPassword/ForgotPassword.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useState } from 'react'
 import { HeadersMeta } from '@/common/components'
 import { NextPageWithLayout } from '@/pages/_app'
 import { Button, Card, Typography, TypographyVariant } from '@nikolajk2/lib-insta-leaders'
@@ -13,16 +13,22 @@ import {
 import { zodResolver } from '@hookform/resolvers/zod'
 import Link from 'next/link'
 import { ROUTES_AUTH } from '@/appRoot/routes/routes'
+import { SendLinkResponseError } from '@/features/auth/ui/forgotPassword/forgotPassword.types'
 
 export const ForgotPassword: NextPageWithLayout = () => {
-  const { handleSubmit, control } = useForm<ForgotPasswordZodSchemaFields>({
+  const { handleSubmit, control, setError } = useForm<ForgotPasswordZodSchemaFields>({
     resolver: zodResolver(ForgotPasswordZodSchema),
   })
+
+  const [sendLinkSuccess, setSendLinkSuccess] = useState<boolean>(false)
+
   const [sendLink] = useForgotPasswordMutation()
 
   const { executeRecaptcha } = useGoogleReCaptcha() // Use reCAPTCHA v3 hook
 
   const onSubmit = handleSubmit(async ({ email }) => {
+    setSendLinkSuccess(false)
+
     if (!executeRecaptcha) {
       console.log('reCAPTCHA has not been loaded yet')
       return
@@ -32,11 +38,18 @@ export const ForgotPassword: NextPageWithLayout = () => {
       // Execute reCAPTCHA and get the token
       const recaptchaToken = await executeRecaptcha('forgot_password')
 
-      // Call your API with the email and reCAPTCHA token
+      // Call the API with the email and reCAPTCHA token
       const res = await sendLink({ email, recaptchaValue: recaptchaToken }).unwrap()
-      console.log(res)
-    } catch (e) {
-      console.log(e)
+
+      // In case of success response
+      if (res.status === 'success') {
+        setSendLinkSuccess(true)
+      }
+    } catch (e: SendLinkResponseError | any) {
+      setError('email', {
+        type: 'manual',
+        message: e?.data?.message || 'Something went wrong',
+      })
     }
   })
 
@@ -66,8 +79,16 @@ export const ForgotPassword: NextPageWithLayout = () => {
             Enter your email address and we will send you further instructions
           </Typography>
 
+          {sendLinkSuccess && (
+            <Typography
+              variant={TypographyVariant.regular_text_14}
+              className={'text-light-100 mt-[23px]'}
+            >
+              The link has been sent by email. If you don’t receive an email send link again
+            </Typography>
+          )}
           <Button variant={'primary'} className={'mt-[17px] h-[36px]'} fullWidth type="submit">
-            Send Link
+            {sendLinkSuccess ? 'Send Link Again' : 'Send Link'}
           </Button>
 
           <Button asChild variant={'text'} className={'mt-[24px] h-[36px]'} fullWidth>
@@ -80,72 +101,3 @@ export const ForgotPassword: NextPageWithLayout = () => {
     </div>
   )
 }
-
-
-// import React from 'react'
-// import { HeadersMeta } from '@/common/components'
-// import { NextPageWithLayout } from '@/pages/_app'
-// import { Button, Card, Typography, TypographyVariant } from '@nikolajk2/lib-insta-leaders'
-// import { FormInput } from '@/common/components/ControllerInput/ControllerInput'
-// import { useForm } from 'react-hook-form'
-// import { useForgotPasswordMutation } from '@/features/auth/ui/forgotPassword/forgotPasswordApi'
-//
-// export const ForgotPassword: NextPageWithLayout = () => {
-//   const { handleSubmit, control, getValues } = useForm()
-//
-//   const [sendLink] = useForgotPasswordMutation()
-//
-//   const onSubmit = handleSubmit(async ({ email, ...rest }) => {
-//     try {
-//       const res = await sendLink({ email, recaptchaValue: '' }).unwrap()
-//       console.log(res)
-//     } catch (e) {
-//       console.log(e)
-//     }
-//   })
-//
-//   return (
-//     <div className={'flex justify-center'}>
-//       <HeadersMeta
-//         title={'Forgot Password'}
-//         description={'Reset your password if you have forgotten it'}
-//       />
-//       <Card className={'!w-378 flex flex-col items-center px-[24px] py-[23px]'}>
-//         <Typography variant={TypographyVariant.h1} className={'text-light-100'}>
-//           Forgot Password
-//         </Typography>
-//
-//         <form onSubmit={onSubmit} className={'mt-[37px] !w-330'}>
-//           <FormInput
-//             name={'email'}
-//             label={'Email'}
-//             control={control}
-//             placeholder={'Epam@epam.com'}
-//           />
-//
-//           <Typography
-//             variant={TypographyVariant.regular_text_14}
-//             className={'mt-[10px] text-light-900'}
-//           >
-//             Enter your email address and we will send you further instructions
-//           </Typography>
-//
-//           {/*<Typography*/}
-//           {/*  variant={TypographyVariant.regular_text_14}*/}
-//           {/*  className={'text-light-100 mt-[23px]'}*/}
-//           {/*>*/}
-//           {/*  The link has been sent by email. If you don’t receive an email send link again*/}
-//           {/*</Typography>*/}
-//
-//           <Button variant={'primary'} className={'mt-[17px] h-[36px]'} fullWidth>
-//             Send Link
-//           </Button>
-//
-//           <Button variant={'text'} className={'mt-[24px] h-[36px]'} fullWidth>
-//             Back to Sign In
-//           </Button>
-//         </form>
-//       </Card>
-//     </div>
-//   )
-// }
--- src/features/auth/ui/forgotPassword/forgotPasswordApi.ts
+++ src/features/auth/ui/forgotPassword/forgotPasswordApi.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import {SendLinkArgs, SendLinkResponse} from "@/features/auth/ui/forgotPassword/forgotPassword.types";
 
 const AUTH = 'v1/auth'
 
@@ -6,7 +7,7 @@ export const forgotPasswordApi = createApi({
   reducerPath: 'forgotPasswordApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://main.sociable-people.com/api' }),
   endpoints: builder => ({
-    forgotPassword: builder.mutation<any, { email: string; recaptchaValue: string }>({
+    forgotPassword: builder.mutation<SendLinkResponse, SendLinkArgs>({
       query: body => ({
         method: 'POST',
         url: `${AUTH}/password-recovery`,
