37fc768cf97cda29878e19084df3a81fa41475b0
klrotex <klrotex11@gmail.com>
2024-10-06 19:07:42 +0300

    refactor: Sign in form change TextField to FormInput

--- src/features/auth/ui/signIn/signInForm/SignInForm.tsx
+++ src/features/auth/ui/signIn/signInForm/SignInForm.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField, Typography, TypographyVariant } from '@nikolajk2/lib-insta-leaders'
+import { Button, Typography, TypographyVariant } from '@nikolajk2/lib-insta-leaders'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { useRouter } from 'next/router'
@@ -6,15 +6,16 @@ import { useLoginMutation } from '@/features/auth/api/authService'
 import { LoginFields, LoginSchema } from './signInFormSchema'
 import { ROUTES_APP, ROUTES_AUTH } from '@/appRoot/routes/routes'
 import Link from 'next/link'
+import { FormInput } from '@/common/components'
 
 export const SignInForm = () => {
   const [signIn, { data, isError, isLoading }] = useLoginMutation()
   const router = useRouter()
 
   const {
-    register,
     handleSubmit,
     formState: { errors },
+    control,
   } = useForm<LoginFields>({ resolver: zodResolver(LoginSchema) })
 
   const onSubmit = handleSubmit(async data => {
@@ -32,17 +33,13 @@ export const SignInForm = () => {
 
   return (
     <form className={'text-left'} onSubmit={onSubmit}>
-      <TextField
-        label={'Email'}
-        type={'email'}
-        className={'mb-6'}
-        {...register('email')}
-        errorMessage={errors.email?.message}
-      />
-      <TextField
+      <FormInput name={'email'} control={control} label={'Email'} className={'mb-6'} />
+      <FormInput
+        name={'password'}
+        control={control}
         label={'Password'}
+        className={'mb-6'}
         password
-        {...register('password')}
         errorMessage={
           isError
             ? 'The email or password are incorrect. Try again please'
