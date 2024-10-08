2b05969beda8b492f3c300b2b174fe79888e8990
k2 <izmail.k2@gmail.com>
2024-10-08 21:33:26 +0300

    feat: add baseQueryWithReauth for refresh token

---
+++ src/appRoot/services/instaLeadersApiWithReauth.ts
@@ -0,0 +1,60 @@
+import { fetchBaseQuery } from '@reduxjs/toolkit/query'
+import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query'
+import { Mutex } from 'async-mutex'
+import { LocalStorageUtil } from '@/common/utils/LocalStorageUtil'
+import Router from 'next/router'
+import { ROUTES_AUTH } from '@/appRoot/routes/routes'
+
+// create a new mutex
+const mutex = new Mutex()
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'https://main.sociable-people.com/api',
+  prepareHeaders: headers => {
+    const token = LocalStorageUtil.getValue('accessToken')
+
+    if (token) {
+      headers.set('Authorization', `Bearer ${token}`)
+    }
+    return headers
+  },
+})
+
+export const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  // wait until the mutex is available without locking it
+  await mutex.waitForUnlock()
+
+  let result = await baseQuery(args, api, extraOptions) //делаем запрос
+  if (result.error && result.error.status === 401) {
+    // checking whether the mutex is locked
+    if (!mutex.isLocked()) {
+      const release = await mutex.acquire()
+      try {
+        const refreshResult = (await baseQuery(
+          { url: '/refresh-token', method: 'POST' },
+          api,
+          extraOptions
+        )) as any
+        if (refreshResult.data) {
+          LocalStorageUtil.setValue('accessToken', refreshResult.data.accessToken)
+          // retry the initial query
+          result = await baseQuery(args, api, extraOptions)
+        } else {
+          await Router.push(ROUTES_AUTH.LOGIN)
+        }
+      } finally {
+        // release must be called once the mutex should be released again.
+        release()
+      }
+    } else {
+      // wait until the mutex is available without locking it
+      await mutex.waitForUnlock()
+      result = await baseQuery(args, api, extraOptions)
+    }
+  }
+  return result
+}
