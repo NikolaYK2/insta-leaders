3d7b4e89735689f79f7ec05918b82d0a8cbd0a7d
albogachiev <albogachiev-2014@mail.ru>
2024-10-08 19:54:16 +0300

    IL-74 fix: fixed util LocalStorageUti

--- src/common/utils/LocalStorageUtil.ts
+++ src/common/utils/LocalStorageUtil.ts
@@ -1,13 +1,36 @@
-const StorageKey = {
-  email: 'email',
+type StorageKeys = {
+  email: 'email'
+  latestCSRFToken: 'latestCSRFToken'
+  userDataGoggle: 'userDataGoggle'
+  userData: 'userData'
 }
+type ParamsKey = keyof StorageKeys
 
 export class LocalStorageUtil {
-  static setEmail(email: string) {
-    localStorage.setItem(StorageKey.email, email)
+  static setValue<T>(key: ParamsKey, value: T): void {
+    try {
+      localStorage.setItem(key, JSON.stringify(value))
+    } catch (e) {
+      console.error('Error saving to localStorage', e)
+    }
   }
 
-  static getEmail() {
-    return localStorage.getItem(StorageKey.email)
+  static getValue<T>(key: ParamsKey): T | null {
+    try {
+      const item = localStorage.getItem(key)
+
+      return item ? (JSON.parse(item) as T) : null
+    } catch (e) {
+      console.error('Error getting from localStorage', e)
+      return null
+    }
+  }
+
+  static removeItem(key: ParamsKey): void {
+    try {
+      localStorage.removeItem(key)
+    } catch (e) {
+      console.error('Error removing from localStorage', e)
+    }
   }
 }
