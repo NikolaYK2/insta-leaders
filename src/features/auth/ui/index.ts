b8d018c45b3bfe2863d710f2403c71a58559b478
k2 <izmail.k2@gmail.com>
2024-10-08 18:50:13 +0300

    feat: add rout google

--- src/features/auth/ui/index.ts
+++ src/features/auth/ui/index.ts
@@ -1,5 +1,5 @@
 export * from './signUp/SignUp'
-export * from './signIn/SignIn'
+export * from './signIn'
 export * from './forgotPassword/ForgotPassword'
 export * from './createNewPassword/CreateNewPassword'
 export * from './passwordRecovery/PasswordRecovery'
---
+++ src/pages/google/index.tsx
@@ -0,0 +1,5 @@
+import { getLayout } from '@/common/components'
+import { SignInGoogle } from '@/features/auth/ui'
+
+SignInGoogle.getLayout = getLayout
+export default SignInGoogle
