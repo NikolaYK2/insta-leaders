71a6a5601d8758f7a4393a1eb27240c4c9c314fb
k2 <izmail.k2@gmail.com>
2024-10-08 18:50:47 +0300

    feat: add types

--- src/features/auth/api/authService.types.ts
+++ src/features/auth/api/authService.types.ts
@@ -3,18 +3,38 @@ export type RegistrationProps = {
   password: string
   email: string
 }
+
+export type Data = {
+  accessToken: string
+  user: DataUser
+}
+
+export type DataUser = {
+  id: number
+  email: string
+  name: string
+}
+
 export type RegistrationResponse = {
   status: string
   code: number
-  data: {
-    id: number
-    email: string
-    name: string
-  }
+  data: DataUser
 }
 
 export type ConfirmEmailResponse = {
   status: string
   code: number
   data: string
+}
+
+export type ProviderRes = {
+  status: string
+  code: number
+  data: Data
+}
+
+export type AuthRes<T = {}> = {
+  status: string
+  code: number
+  data: T
 }
