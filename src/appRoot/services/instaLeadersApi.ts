501a31aad317c398cd925bf0837ee5f89acbf746
klrotex <klrotex11@gmail.com>
2024-10-09 18:02:56 +0300

    refactor: temp baseQuery

--- src/appRoot/services/instaLeadersApi.ts
+++ src/appRoot/services/instaLeadersApi.ts
@@ -1,9 +1,9 @@
-import { createApi } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { baseQueryWithReauth } from '@/appRoot/services/instaLeadersApiWithReauth'
 
 // Define a service using a base URL and expected endpoints
 export const instaLeadersApi = createApi({
-  baseQuery: baseQueryWithReauth,
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://main.sociable-people.com/api' }),
   endpoints: () => ({}),
   reducerPath: 'instaLeadersApi',
   tagTypes: ['Auth'],
