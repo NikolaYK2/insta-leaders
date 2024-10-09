7d454a8d17a0363f3d933f95729dfff70a5e2434
albogachiev <albogachiev-2014@mail.ru>
2024-10-09 11:52:30 +0300

    IL-89 feat: added a new custom hook useLogOut

--- src/features/auth/ui/logOut/logOut.tsx
+++ src/features/auth/ui/logOut/logOut.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { DynamicIcon, Typography, TypographyVariant } from '@nikolajk2/lib-insta-leaders'
+import { useLogOut } from '@/common/utils/useLogOut'
 
 const LogOut = () => {
+  const { onLogOut } = useLogOut()
+
   return (
-    <button className={'flex items-center gap-3'}>
+    <button onClick={onLogOut} className={'flex items-center gap-3'}>
       <DynamicIcon iconId={'LogOut'} width={'24'} height={'24'} />
       <Typography variant={TypographyVariant.medium_text_14}>Log Out</Typography>
     </button>
