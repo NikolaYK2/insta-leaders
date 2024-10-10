938c333bb458c347160b9a523a3f7292ad860670
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-10-11 01:02:09 +0400

    IL-72.2 fix ForgotPassword api

--- src/features/auth/api/authService.ts
+++ src/features/auth/api/authService.ts
@@ -1,84 +1,94 @@
-import { instaLeadersApi } from '@/appRoot/services/instaLeadersApi'
+import {instaLeadersApi} from '@/appRoot/services/instaLeadersApi'
 import {
-  ConfirmEmailResponse,
-  LogOutResponse,
-  LoginArgs,
-  LoginResponse,
-  RegistrationProps,
-  RegistrationResponse,
+    ConfirmEmailResponse,
+    LogOutResponse,
+    LoginArgs,
+    LoginResponse,
+    RegistrationProps,
+    RegistrationResponse, SendLinkResponse, SendLinkArgs,
 } from '@/features/auth/api/authService.types'
-import { LocalStorageUtil } from '@/common/utils/LocalStorageUtil'
+import {LocalStorageUtil} from '@/common/utils/LocalStorageUtil'
 
 const AUTH = 'v1/auth'
 const authService = instaLeadersApi.injectEndpoints({
-  endpoints: builder => ({
-    registration: builder.mutation<RegistrationResponse, RegistrationProps>({
-      query: body => ({
-        method: 'POST',
-        url: `${AUTH}/registration`,
-        body,
-      }),
-      async onQueryStarted(_, { queryFulfilled }) {
-        const { data } = await queryFulfilled
-        const email = data.data.email
-        if (!email) return
-        LocalStorageUtil.setValue<string>('email', data.data.email)
-      },
+    endpoints: builder => ({
+        registration: builder.mutation<RegistrationResponse, RegistrationProps>({
+            query: body => ({
+                method: 'POST',
+                url: `${AUTH}/registration`,
+                body,
+            }),
+            async onQueryStarted(_, {queryFulfilled}) {
+                const {data} = await queryFulfilled
+                const email = data.data.email
+                if (!email) return
+                LocalStorageUtil.setValue<string>('email', data.data.email)
+            },
+        }),
+        confirmEmail: builder.mutation<ConfirmEmailResponse, string>({
+            query: code => ({
+                method: 'GET',
+                url: `${AUTH}/email-confirmation`,
+                params: {code},
+            }),
+        }),
+        resendEmail: builder.mutation<ConfirmEmailResponse, { email: string }>({
+            query: body => ({
+                method: 'POST',
+                url: `${AUTH}/registration-email-resending`,
+                body,
+            }),
+        }),
+        login: builder.mutation<LoginResponse, LoginArgs>({
+            query: arg => {
+                return {
+                    url: `${AUTH}/login`,
+                    method: 'POST',
+                    body: arg,
+                }
+            },
+        }),
+        authByGithub: builder.query<LoginResponse, { code: string }>({
+            query: arg => {
+                return {
+                    url: `${AUTH}/registration/by-github?code=${arg.code}`,
+                }
+            },
+        }),
+        logOut: builder.mutation<LogOutResponse, void>({
+            query: () => ({
+                method: 'POST',
+                url: `${AUTH}/logout`,
+                // credentials: 'include',
+            }),
+        }),
+        createNewPassword: builder.mutation<any, { newPassword: string; recoveryCode: string }>({
+            query: body => ({
+                method: 'POST',
+                url: `${AUTH}/new-password`,
+                body,
+            }),
+        }),
+        forgotPassword: builder.mutation<SendLinkResponse, SendLinkArgs>({
+            query: body => ({
+                method: 'POST',
+                url: `${AUTH}/password-recovery`,
+                body,
+            }),
+        }),
+
     }),
-    confirmEmail: builder.mutation<ConfirmEmailResponse, string>({
-      query: code => ({
-        method: 'GET',
-        url: `${AUTH}/email-confirmation`,
-        params: { code },
-      }),
-    }),
-    resendEmail: builder.mutation<ConfirmEmailResponse, { email: string }>({
-      query: body => ({
-        method: 'POST',
-        url: `${AUTH}/registration-email-resending`,
-        body,
-      }),
-    }),
-    login: builder.mutation<LoginResponse, LoginArgs>({
-      query: arg => {
-        return {
-          url: `${AUTH}/login`,
-          method: 'POST',
-          body: arg,
-        }
-      },
-    }),
-    authByGithub: builder.query<LoginResponse, { code: string }>({
-      query: arg => {
-        return {
-          url: `${AUTH}/registration/by-github?code=${arg.code}`,
-        }
-      },
-    }),
-    logOut: builder.mutation<LogOutResponse, void>({
-      query: () => ({
-        method: 'POST',
-        url: `${AUTH}/logout`,
-        // credentials: 'include',
-      }),
-    }),
-    createNewPassword: builder.mutation<any, { newPassword: string; recoveryCode: string }>({
-      query: body => ({
-        method: 'POST',
-        url: `${AUTH}/new-password`,
-        body,
-      }),
-    }),
-  }),
-  overrideExisting: true,
+    overrideExisting: true,
 })
 //пример
 export const {
-  useRegistrationMutation,
-  useConfirmEmailMutation,
-  useResendEmailMutation,
-  useLogOutMutation,
-  useCreateNewPasswordMutation,
-  useLoginMutation,
-  useAuthByGithubQuery,
+    useRegistrationMutation,
+    useConfirmEmailMutation,
+    useResendEmailMutation,
+    useLogOutMutation,
+    useCreateNewPasswordMutation,
+    useLoginMutation,
+    useAuthByGithubQuery,
+    useForgotPasswordMutation,
 } = authService
+
--- src/features/auth/api/authService.types.ts
+++ src/features/auth/api/authService.types.ts
@@ -44,3 +44,28 @@ export type LogOutResponse = {
   code: number
   data: null
 }
+
+
+export type SendLinkArgs = {
+  email: string
+  recaptchaValue: string
+}
+
+export type SendLinkResponse = SendLinkResponseSuccess | SendLinkResponseError
+
+export type SendLinkResponseSuccess = {
+  code: number
+  status: string
+  data: {
+    recoveryCode: string // !!! Should be revised by Backend
+  }
+}
+
+export type SendLinkResponseError = {
+  status: number
+  data: {
+    status: string
+    code: number
+    message: string
+  }
+}
