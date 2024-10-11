b63d1066ff3d3639672ea2f1d84a590bf2337d96
k2 <izmail.k2@gmail.com>
2024-10-11 23:25:55 +0300

    feat: add authByGoogle service

--- src/features/auth/api/authService.ts
+++ src/features/auth/api/authService.ts
@@ -1,94 +1,103 @@
-import {instaLeadersApi} from '@/appRoot/services/instaLeadersApi'
+import { instaLeadersApi } from '@/appRoot/services/instaLeadersApi'
 import {
-    ConfirmEmailResponse,
-    LogOutResponse,
-    LoginArgs,
-    LoginResponse,
-    RegistrationProps,
-    RegistrationResponse, SendLinkResponse, SendLinkArgs,
+  ConfirmEmailResponse,
+  LogOutResponse,
+  LoginArgs,
+  LoginResponse,
+  RegistrationProps,
+  RegistrationResponse,
+  SendLinkResponse,
+  SendLinkArgs,
 } from '@/features/auth/api/authService.types'
-import {LocalStorageUtil} from '@/common/utils/LocalStorageUtil'
+import { LocalStorageUtil } from '@/common/utils/LocalStorageUtil'
 
 const AUTH = 'v1/auth'
 const authService = instaLeadersApi.injectEndpoints({
-    endpoints: builder => ({
-        registration: builder.mutation<RegistrationResponse, RegistrationProps>({
-            query: body => ({
-                method: 'POST',
-                url: `${AUTH}/registration`,
-                body,
-            }),
-            async onQueryStarted(_, {queryFulfilled}) {
-                const {data} = await queryFulfilled
-                const email = data.data.email
-                if (!email) return
-                LocalStorageUtil.setValue<string>('email', data.data.email)
-            },
-        }),
-        confirmEmail: builder.mutation<ConfirmEmailResponse, string>({
-            query: code => ({
-                method: 'GET',
-                url: `${AUTH}/email-confirmation`,
-                params: {code},
-            }),
-        }),
-        resendEmail: builder.mutation<ConfirmEmailResponse, { email: string }>({
-            query: body => ({
-                method: 'POST',
-                url: `${AUTH}/registration-email-resending`,
-                body,
-            }),
-        }),
-        login: builder.mutation<LoginResponse, LoginArgs>({
-            query: arg => {
-                return {
-                    url: `${AUTH}/login`,
-                    method: 'POST',
-                    body: arg,
-                }
-            },
-        }),
-        authByGithub: builder.query<LoginResponse, { code: string }>({
-            query: arg => {
-                return {
-                    url: `${AUTH}/registration/by-github?code=${arg.code}`,
-                }
-            },
-        }),
-        logOut: builder.mutation<LogOutResponse, void>({
-            query: () => ({
-                method: 'POST',
-                url: `${AUTH}/logout`,
-                // credentials: 'include',
-            }),
-        }),
-        createNewPassword: builder.mutation<any, { newPassword: string; recoveryCode: string }>({
-            query: body => ({
-                method: 'POST',
-                url: `${AUTH}/new-password`,
-                body,
-            }),
-        }),
-        forgotPassword: builder.mutation<SendLinkResponse, SendLinkArgs>({
-            query: body => ({
-                method: 'POST',
-                url: `${AUTH}/password-recovery`,
-                body,
-            }),
-        }),
-
+  endpoints: builder => ({
+    registration: builder.mutation<RegistrationResponse, RegistrationProps>({
+      query: body => ({
+        method: 'POST',
+        url: `${AUTH}/registration`,
+        body,
+      }),
+      async onQueryStarted(_, { queryFulfilled }) {
+        const { data } = await queryFulfilled
+        const email = data.data.email
+        if (!email) return
+        LocalStorageUtil.setValue<string>('email', data.data.email)
+      },
     }),
-    overrideExisting: true,
+    confirmEmail: builder.mutation<ConfirmEmailResponse, string>({
+      query: code => ({
+        method: 'GET',
+        url: `${AUTH}/email-confirmation`,
+        params: { code },
+      }),
+    }),
+    resendEmail: builder.mutation<ConfirmEmailResponse, { email: string }>({
+      query: body => ({
+        method: 'POST',
+        url: `${AUTH}/registration-email-resending`,
+        body,
+      }),
+    }),
+    login: builder.mutation<LoginResponse, LoginArgs>({
+      query: arg => {
+        return {
+          url: `${AUTH}/login`,
+          method: 'POST',
+          body: arg,
+        }
+      },
+    }),
+    authByGithub: builder.query<LoginResponse, { code: string }>({
+      query: arg => {
+        return {
+          url: `${AUTH}/registration/by-github?code=${arg.code}`,
+        }
+      },
+    }),
+    authByGoogle: builder.query<LoginResponse, { provider: 'google'; code: string }>({
+      query: params => {
+        return {
+          url: `${AUTH}/registration/by-provider`,
+          params,
+        }
+      },
+    }),
+    logOut: builder.mutation<LogOutResponse, void>({
+      query: () => ({
+        method: 'POST',
+        url: `${AUTH}/logout`,
+        // credentials: 'include',
+      }),
+    }),
+    createNewPassword: builder.mutation<any, { newPassword: string; recoveryCode: string }>({
+      query: body => ({
+        method: 'POST',
+        url: `${AUTH}/new-password`,
+        body,
+      }),
+    }),
+    forgotPassword: builder.mutation<SendLinkResponse, SendLinkArgs>({
+      query: body => ({
+        method: 'POST',
+        url: `${AUTH}/password-recovery`,
+        body,
+      }),
+    }),
+  }),
+  overrideExisting: true,
 })
 //пример
 export const {
-    useRegistrationMutation,
-    useConfirmEmailMutation,
-    useResendEmailMutation,
-    useLogOutMutation,
-    useCreateNewPasswordMutation,
-    useLoginMutation,
-    useAuthByGithubQuery,
-    useForgotPasswordMutation,
+  useRegistrationMutation,
+  useConfirmEmailMutation,
+  useResendEmailMutation,
+  useLogOutMutation,
+  useCreateNewPasswordMutation,
+  useLoginMutation,
+  useAuthByGithubQuery,
+  useForgotPasswordMutation,
+  useAuthByGoogleQuery,
 } = authService
-
