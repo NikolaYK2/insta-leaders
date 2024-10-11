29a5ed00fcd9edf664186a8fed81447ce0a3dce2
k2 <izmail.k2@gmail.com>
2024-10-11 23:26:54 +0300

    feat: add AuthByGoogle

--- src/features/auth/ui/signIn/SignIn.tsx
+++ src/features/auth/ui/signIn/SignIn.tsx
@@ -1,23 +1,13 @@
 import { HeadersMeta } from '@/common/components'
 import { NextPageWithLayout } from '@/pages/_app'
-import {
-  Button,
-  Card,
-  DynamicIcon,
-  Typography,
-  TypographyVariant,
-} from '@nikolajk2/lib-insta-leaders'
+import { Button, Card, Typography, TypographyVariant } from '@nikolajk2/lib-insta-leaders'
 import Link from 'next/link'
 import { ROUTES_AUTH } from '@/appRoot/routes/routes'
 import { SignInForm } from './signInForm/SignInForm'
 import { AuthByGithub } from './authByGithub/AuthByGithub'
+import { AuthByGoogle } from '@/features/auth/ui/signIn/authByGoogle/AuthByGoogle'
 
 export const SignIn: NextPageWithLayout = () => {
-  const client_id = '792546249106-u5of55jk4hus635kpd936g5968b62a1c.apps.googleusercontent.com'
-  const redirect_uri = 'http://localhost:3000/google'
-  const state = '50c45fc5314190fc5d117c09dc9ebadf'
-  const linkToGoogleLogin = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${client_id}&response_type=code&scope=email profile&redirect_uri=${redirect_uri}&state=${state}`
-
   return (
     <>
       <HeadersMeta title={'Sign In'} description={'Access your account by signing in'} />
@@ -28,9 +18,7 @@ export const SignIn: NextPageWithLayout = () => {
           </Typography>
 
           <div className={'w-[132px] m-auto mb-6 mt-3 flex justify-between'}>
-            <Link className={'flex items-center'} href={linkToGoogleLogin}>
-              <DynamicIcon iconId={'GoogleSvgrepoCom1'} width={36} height={36} />
-            </Link>
+            <AuthByGoogle />
             <AuthByGithub />
           </div>
           <SignInForm />
