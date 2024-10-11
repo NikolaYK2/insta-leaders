a7fefc5245de1bfc02a3bcfd146638f35b20345d
k2 <izmail.k2@gmail.com>
2024-10-11 23:23:59 +0300

    feat: add env.d.ts fro env inv

---
+++ env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: string;
+    NEXT_PUBLIC_BASE_URL: string; // Другие переменные окружения, которые вам нужны
+  }
+}
