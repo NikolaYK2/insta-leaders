fa63cb8098b613a0a9560f63bddc0b7c6604087f
k2 <izmail.k2@gmail.com>
2024-10-12 18:26:00 +0300

    refactor: remove block h2

--- src/features/userHub/ui/myProfile/MyProfile.tsx
+++ src/features/userHub/ui/myProfile/MyProfile.tsx
@@ -9,49 +9,49 @@ export const MyProfile: NextPageWithLayout = () => {
       titleMeta={'My Profile'}
       descriptionMeta={'View and edit your personal profile information'}
     >
-      <h2>My Profile</h2>
       <section className={'flex justify-between h-[204px] mb-12'}>
         <div className={'border-2 border-cyan-50 max-w-[204px] w-full'}>
           <div className={'w-full h-full border-2 border-red-800 rounded-full'}></div>
         </div>
-      <section className={'flex justify-between h-[204px] mb-12'}>
-        <div className={'border-2 border-cyan-50 max-w-[204px] w-full'}></div>
-        <div className={'border-2 border-cyan-50 w-full max-w-[730px]'}>
-          <div className={'flex justify-between items-center border-2 border-red-800 mb-5'}>
-            <Typography>URLProfile</Typography>
-            <Button variant={'secondary'}>
-              <Typography variant={TypographyVariant.h3}>Profile Settings</Typography>
-            </Button>
-          </div>
+        <section className={'flex justify-between h-[204px] mb-12'}>
+          <div className={'border-2 border-cyan-50 max-w-[204px] w-full'}></div>
+          <div className={'border-2 border-cyan-50 w-full max-w-[730px]'}>
+            <div className={'flex justify-between items-center border-2 border-red-800 mb-5'}>
+              <Typography>URLProfile</Typography>
+              <Button variant={'secondary'}>
+                <Typography variant={TypographyVariant.h3}>Profile Settings</Typography>
+              </Button>
+            </div>
 
-          <div className={'flex flex-row border-2 border-green-500 mb-7'}>
-            <div className={'border-2 border-red-800 max-w-[159px] w-full mr-1'}>
-              <Typography variant={TypographyVariant.bold_text_14}>2000</Typography>
-              <Typography variant={TypographyVariant.regular_text_14}>bla bla</Typography>
+            <div className={'flex flex-row border-2 border-green-500 mb-7'}>
+              <div className={'border-2 border-red-800 max-w-[159px] w-full mr-1'}>
+                <Typography variant={TypographyVariant.bold_text_14}>2000</Typography>
+                <Typography variant={TypographyVariant.regular_text_14}>bla bla</Typography>
+              </div>
+              <div className={'border-2 border-red-800 max-w-[139px] w-full mr-1'}>
+                <Typography variant={TypographyVariant.bold_text_14}>2000</Typography>
+                <Typography variant={TypographyVariant.regular_text_14}>bla bla</Typography>
+              </div>
+              <div className={'border-2 border-red-800'}>
+                <Typography variant={TypographyVariant.bold_text_14}>2000</Typography>
+                <Typography variant={TypographyVariant.regular_text_14}>bla bla</Typography>
+              </div>
             </div>
-            <div className={'border-2 border-red-800 max-w-[139px] w-full mr-1'}>
-              <Typography variant={TypographyVariant.bold_text_14}>2000</Typography>
-              <Typography variant={TypographyVariant.regular_text_14}>bla bla</Typography>
-            </div>
-            <div className={'border-2 border-red-800'}>
-              <Typography variant={TypographyVariant.bold_text_14}>2000</Typography>
-              <Typography variant={TypographyVariant.regular_text_14}>bla bla</Typography>
-            </div>
-          </div>
-          <Typography
-            variant={TypographyVariant.regular_text_16}
-            className={'border-2 border-yellow-400'}
-          >
-            bla bla bla bla
             <Typography
-              asChild
-              variant={TypographyVariant.regular_link}
-              className={'cursor-pointer'}
+              variant={TypographyVariant.regular_text_16}
+              className={'border-2 border-yellow-400'}
             >
-              <span>span span span</span>
+              bla bla bla bla
+              <Typography
+                asChild
+                variant={TypographyVariant.regular_link}
+                className={'cursor-pointer'}
+              >
+                <span>span span span</span>
+              </Typography>
             </Typography>
-          </Typography>
-        </div>
+          </div>
+        </section>
       </section>
       <section className={'border-2 border-red-800'}>items</section>
     </Page>
