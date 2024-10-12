6c1428bf1c98ccd3efe48ff49065e3c9e7efdf33
k2 <izmail.k2@gmail.com>
2024-10-12 15:50:54 +0300

    style: add border right

--- src/common/components/Sidebar/Sidebar.tsx
+++ src/common/components/Sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ import LogOut from '@/features/auth/ui/logOut/logOut'
 
 export const Sidebar = () => {
   return (
-    <div className="flex flex-col align-sub w-64 bg-gray-800 text-white">
+    <div className="flex flex-col align-sub w-64 text-white border-r-[1px] border-dark-300">
       <Link href={ROUTES_APP.HOME}>Home</Link>
       <Link href={ROUTES_APP.CREATE}>Create</Link>
       <Link href={ROUTES_APP.PROFILE}>My Profile</Link>
