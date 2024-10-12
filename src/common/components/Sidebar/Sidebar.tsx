4fe5acef40487cfeb38b7368a0b664e3320a250b
k2 <izmail.k2@gmail.com>
2024-10-12 17:25:58 +0300

    style: width

--- src/common/components/Sidebar/Sidebar.tsx
+++ src/common/components/Sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ import LogOut from '@/features/auth/ui/logOut/logOut'
 
 export const Sidebar = () => {
   return (
-    <div className="flex flex-col align-sub w-64 text-white border-r-[1px] border-dark-300">
+    <div className="flex flex-col align-sub max-w-[220px] w-full text-white border-r-[1px] border-dark-300">
       <Link href={ROUTES_APP.HOME}>Home</Link>
       <Link href={ROUTES_APP.CREATE}>Create</Link>
       <Link href={ROUTES_APP.PROFILE}>My Profile</Link>
