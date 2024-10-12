2c85e1445269dd31c52711292d324c299fda0bdb
k2 <izmail.k2@gmail.com>
2024-10-12 16:59:49 +0300

    style: blocks

--- src/features/userHub/ui/myProfile/MyProfile.tsx
+++ src/features/userHub/ui/myProfile/MyProfile.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NextPageWithLayout } from '@/pages/_app'
 import { Page } from '@/common/components/page'
+import { Typography } from '@nikolajk2/lib-insta-leaders'
 
 export const MyProfile: NextPageWithLayout = () => {
   return (
@@ -9,6 +10,15 @@ export const MyProfile: NextPageWithLayout = () => {
       descriptionMeta={'View and edit your personal profile information'}
     >
       <h2>My Profile</h2>
+      <section className={'flex'}>
+        <div className={'border-2 border-cyan-50 max-w-[204px] h-[204px] w-full'}></div>
+        <div className={'border-2 border-cyan-50 w-full max-w-[730px]'}>
+          <div className={'border-2 border-red-800'}></div>
+          <div className={'border-2 border-green-500'}></div>
+          <Typography className={'border-2 border-yellow-400'}></Typography>
+        </div>
+      </section>
+      <section></section>
     </Page>
   )
 }
