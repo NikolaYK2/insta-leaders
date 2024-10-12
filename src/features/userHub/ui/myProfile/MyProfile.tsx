3074cd194ddb5e8d8ebb0cbf9653686b7b979d70
k2 <izmail.k2@gmail.com>
2024-10-12 16:13:33 +0300

    refactor: add Page in MyProfile.tsx

--- src/features/userHub/ui/myProfile/MyProfile.tsx
+++ src/features/userHub/ui/myProfile/MyProfile.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import { HeadersMeta } from '@/common/components'
 import { NextPageWithLayout } from '@/pages/_app'
+import { Page } from '@/common/components/page'
 
 export const MyProfile: NextPageWithLayout = () => {
   return (
-    <div>
-      <HeadersMeta
-        title={'My Profile'}
-        description={'View and edit your personal profile information'}
-      />
+    <Page
+      titleMeta={'My Profile'}
+      descriptionMeta={'View and edit your personal profile information'}
+    >
       <h2>My Profile</h2>
-    </div>
+    </Page>
   )
 }
