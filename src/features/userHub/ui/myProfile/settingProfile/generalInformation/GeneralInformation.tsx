9d856fec45a78d2d833a9d7ad803a623673bdcc3
k2 <izmail.k2@gmail.com>
2024-10-12 23:31:51 +0300

    style: block item - temp textarea - div decoration - Button submit

--- src/features/userHub/ui/myProfile/settingProfile/generalInformation/GeneralInformation.tsx
+++ src/features/userHub/ui/myProfile/settingProfile/generalInformation/GeneralInformation.tsx
@@ -6,32 +6,52 @@ import {
   TextField,
   useSelectedCalendar,
   SelectItem,
+  Button,
+  Typography,
+  TypographyVariant,
 } from '@nikolajk2/lib-insta-leaders'
 
+const textFields = [{ label: 'Username*' }, { label: 'First Name*' }, { label: 'Last Name*' }]
 export const GeneralInformation = () => {
   const { selectedDate, setSelectedDate } = useSelectedCalendar()
 
   return (
-    <Page titleMeta={'General information'} descriptionMeta={'info'}>
-      <div>
-        <div></div>
-        <form>
-          <TextField className={'h-9'} />
-          <TextField className={'h-9'} />
-          <TextField className={'h-9'} />
+    <Page titleMeta={'General information'} descriptionMeta={'info'} className={'px-0'}>
+      <div className={'flex justify-between'}>
+        <div className={'border-2 border-red-800 mr-1'}>
+          <div
+            className={
+              'w-[192px] h-[192px] border-2 border-cyan-50 rounded-full mb-[30px] mt-[25px]'
+            }
+          ></div>
+          <Button variant={'outline'}>
+            <Typography variant={TypographyVariant.h3}>Add a Profile Photo</Typography>
+          </Button>
+        </div>
+        <form className={'flex flex-col max-w-[740px] w-full'}>
+          {textFields.map(field => (
+            <TextField className={'mb-6 w-full'} key={field.label} label={field.label} />
+          ))}
           <InputDataPicker
             selected={selectedDate}
             onSelect={setSelectedDate}
             labelInput={'Date of birth'}
+            sideOffsetContent={-19}
           />
-          <div className={'flex'}>
-            <Selector defaultValue={'1'}>
+          <div className={'flex justify-between mt-9'}>
+            <Selector className={'max-w-[358px] mr-1'} defaultValue={'1'}>
               <SelectItem value={'1'} />
             </Selector>
-            <Selector defaultValue={'1'}>
+            <Selector className={'max-w-[358px]'} defaultValue={'1'}>
               <SelectItem value={'1'} />
             </Selector>
           </div>
+
+          <textarea className={'w-full mt-9 bg-dark-300 text-light-100'} />
+          <div className={'border-[1px] border-dark-300 my-7'} />
+          <Button className={'ml-auto'}>
+            <Typography variant={TypographyVariant.h3}>Save Changes</Typography>
+          </Button>
         </form>
       </div>
     </Page>
