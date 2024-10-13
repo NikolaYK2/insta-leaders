77077463467ce690bbf5b19efa70b1bdefb04f9e
k2 <izmail.k2@gmail.com>
2024-10-13 21:19:50 +0300

    feat: add userId for rout profile

--- src/common/components/Sidebar/Sidebar.tsx
+++ src/common/components/Sidebar/Sidebar.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Link from 'next/link'
 import { ROUTES_APP } from '@/appRoot/routes/routes'
 import LogOut from '@/features/auth/ui/logOut/logOut'
+import { LocalStorageUtil } from '@/common/utils/LocalStorageUtil'
 
 export const Sidebar = () => {
+  const userId = LocalStorageUtil.getValue('userId')
+
   return (
     <div className="flex flex-col align-sub max-w-[156px] w-full text-white border-r-[1px] border-dark-300">
       <Link href={ROUTES_APP.HOME}>Home</Link>
       <Link href={ROUTES_APP.CREATE}>Create</Link>
-      <Link href={ROUTES_APP.PROFILE}>My Profile</Link>
+      <Link href={`${ROUTES_APP.PROFILE}/${userId}`}>My Profile</Link>
       <Link href={ROUTES_APP.MESSENGER}>Messenger</Link>
       <Link href={ROUTES_APP.SEARCH}>Search</Link>
       <Link href={ROUTES_APP.STATISTICS}>Statistics</Link>
