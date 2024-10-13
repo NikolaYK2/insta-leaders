f38f34b5c4621baaf1e1a99549e38d3dee2df69f
k2 <izmail.k2@gmail.com>
2024-10-13 19:40:01 +0300

    feat: add res type

--- src/features/userHub/api/user/userService.ts
+++ src/features/userHub/api/user/userService.ts
@@ -1,9 +1,10 @@
 import { instaLeadersApi } from '@/appRoot/services/instaLeadersApi'
+import { Res, UserData } from '@/features/userHub/api/user/userServiceType'
 
 const USERS = 'v1/users'
 const userService = instaLeadersApi.injectEndpoints({
   endpoints: builder => ({
-    getUsersMe: builder.query<void, void>({
+    getUsersMe: builder.query<Res<UserData>, void>({
       query: arg => ({
         url: `${USERS}/me`,
       }),
