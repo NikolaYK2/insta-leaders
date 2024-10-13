6bf4ca98c97f1005a8b65b9ed458f0df48772f9f
k2 <izmail.k2@gmail.com>
2024-10-13 22:49:06 +0300

    feat: add Posts type

--- src/features/userHub/api/user/userServiceType.ts
+++ src/features/userHub/api/user/userServiceType.ts
@@ -1,8 +1,10 @@
+//common-----------
 export type Res<T> = {
   status: string
   code: number
   data: T
 }
+// users----------
 export type UserData = {
   id: number
   email: string
@@ -14,4 +16,16 @@ export type UserData = {
   cityId: number
   aboutMe: string
   avatar: string
+}
+// posts----------------
+export type PostsDataPhotos = {
+  id: number
+  url: string
+}
+export type PostsData = {
+  id: number
+  text: string
+  location: string
+  userId: number
+  photos: PostsDataPhotos[]
 }
