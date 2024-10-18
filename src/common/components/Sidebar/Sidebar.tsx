161cb07bf440ec072bf4539406a3214134e2c105
k2 <izmail.k2@gmail.com>
2024-10-18 16:32:55 +0300

    fix: added type conversion for correct retrieval of userId from localStorage

--- src/common/components/Sidebar/Sidebar.tsx
+++ src/common/components/Sidebar/Sidebar.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { ROUTES_APP } from '@/appRoot/routes/routes'
 import LogOut from '@/features/auth/ui/logOut/logOut'
 import { NextPageWithLayout } from '@/pages/_app'
-import { useGetUsersMeQuery } from '@/features/userHub/api/user/userService'
+
+import { LocalStorageUtil } from '@/common/utils/LocalStorageUtil'
 
 export const Sidebar: NextPageWithLayout = () => {
-  const { data } = useGetUsersMeQuery()
+  const [userId, setUserId] = useState<string | null>(null)
+
+  useEffect(() => {
+    const storedUserId = LocalStorageUtil.getValue('userId') as string | null
+
+    setUserId(storedUserId)
+  }, [])
 
   return (
     <div className="flex flex-col align-sub max-w-[156px] w-full text-white border-r-[1px] border-dark-300">
       <Link href={ROUTES_APP.HOME}>Home</Link>
       <Link href={ROUTES_APP.CREATE}>Create</Link>
-      <Link href={`${ROUTES_APP.PROFILE}/${data?.data.id}`}>My Profile</Link>
+      <Link href={`${ROUTES_APP.PROFILE}/${userId}`}>My Profile</Link>
       <Link href={ROUTES_APP.MESSENGER}>Messenger</Link>
       <Link href={ROUTES_APP.SEARCH}>Search</Link>
       <Link href={ROUTES_APP.STATISTICS}>Statistics</Link>
