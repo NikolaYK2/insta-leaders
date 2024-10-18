3e4961e58a278f9ef196d89eaceb0e0dbd7716cd
k2 <izmail.k2@gmail.com>
2024-10-18 16:33:23 +0300

    feat: google state

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_GOOGLE_CLIENT_ID: string;
+    NEXT_PUBLIC_GOOGLE_STATE: string;
     NEXT_PUBLIC_GITHUB_CLIENT_ID: string;
     NEXT_PUBLIC_BASE_URL: string; // Другие переменные окружения, которые вам нужны
   }
