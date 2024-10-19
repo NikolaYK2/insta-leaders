4fbaefc21a453dd9f9f2b8d173c9645f30f08499
k2 <izmail.k2@gmail.com>
2024-10-19 18:13:25 +0300

    feat: add props

--- src/common/components/ControllerInputDataPicker/ControllerInputDataPicker.tsx
+++ src/common/components/ControllerInputDataPicker/ControllerInputDataPicker.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { FieldValues, useController, UseControllerProps } from 'react-hook-form'
 import { DataType, InputDataPicker, TextFieldProps } from '@nikolajk2/lib-insta-leaders'
 
 type ControllerInputDataPickerProps<TFieldValues extends FieldValues> =
   UseControllerProps<TFieldValues> &
     Omit<TextFieldProps, 'id' | 'onChange'> & {
+      error?: ReactNode
       selected: DataType
       label: string
     }
@@ -14,6 +15,8 @@ export const ControllerInputDataPicker = <TFieldValues extends FieldValues>({
   name,
   selected,
   label,
+  error,
+  disabled,
 }: ControllerInputDataPickerProps<TFieldValues>) => {
   const {
     field: { onChange },
@@ -21,5 +24,13 @@ export const ControllerInputDataPicker = <TFieldValues extends FieldValues>({
     control,
     name,
   })
-  return <InputDataPicker selected={selected} onSelect={onChange} labelInput={label} />
+  return (
+    <InputDataPicker
+      selected={selected}
+      onSelect={onChange}
+      labelInput={label}
+      error={error}
+      disabled={disabled}
+    />
+  )
 }
