b18caa1418396afe8c90bfa99aa8756dae4513a5
k2 <izmail.k2@gmail.com>
2024-10-19 18:12:54 +0300

    feat: added flexibility in the GoBack component for default return
    Added logic to allow default back to previous page (`router.back()`) if no route (`backToRedirect`) is specified.

--- src/common/components/GoBack/GoBack.tsx
+++ src/common/components/GoBack/GoBack.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import { Button, DynamicIcon, Typography, TypographyVariant } from '@nikolajk2/lib-insta-leaders'
-import Link from 'next/link'
 import { cn } from '@/common/utils/cn'
+import { useRouter } from 'next/router'
 
-type GoBackProps = {
+export type GoBackProps = {
   backToTitle?: string
-  backToRedirect: string
+  backToRedirect?: string
 }
 export const GoBack = ({ backToRedirect, backToTitle }: GoBackProps) => {
+  const router = useRouter()
+
+  const handleGoBack = () => {
+    backToRedirect ? router.push(backToRedirect) : router.back()
+  }
+
   return (
-    <Button variant={'text'} asChild className={cn('!p-0 !text-light-100')}>
-      <Link href={backToRedirect}>
-        <DynamicIcon iconId={'ArrowBackOutline'} width={24} height={24} />
-        <Typography variant={TypographyVariant.regular_text_14}>
-          Back to <span className={'capitalize'}>{backToTitle}</span>
-        </Typography>
-      </Link>
+    <Button variant={'text'} className={cn('!p-0 !text-light-100')} onClick={handleGoBack}>
+      <DynamicIcon iconId={'ArrowBackOutline'} width={24} height={24} />
+      <Typography variant={TypographyVariant.regular_text_14}>
+        Back to <span className={'capitalize'}>{backToTitle}</span>
+      </Typography>
     </Button>
   )
 }
