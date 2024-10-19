da24fd3a424fd9c03a4f9c524130b5b2560e3899
k2 <izmail.k2@gmail.com>
2024-10-19 23:36:35 +0300

    refactor: optimize date picker to reduce re-renders with debounce and local state management

--- src/common/components/ControllerInputDataPicker/ControllerInputDataPicker.tsx
+++ src/common/components/ControllerInputDataPicker/ControllerInputDataPicker.tsx
@@ -1,11 +1,12 @@
-import React, { ReactNode } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FieldValues, useController, UseControllerProps } from 'react-hook-form'
 import { DataType, InputDataPicker, TextFieldProps } from '@nikolajk2/lib-insta-leaders'
+import { useDebounce } from '@/common/hooks'
 
 type ControllerInputDataPickerProps<TFieldValues extends FieldValues> =
   UseControllerProps<TFieldValues> &
     Omit<TextFieldProps, 'id' | 'onChange'> & {
-      error?: ReactNode
+      error?: React.ReactNode
       selected: DataType
       label: string
     }
@@ -24,10 +25,34 @@ export const ControllerInputDataPicker = <TFieldValues extends FieldValues>({
     control,
     name,
   })
+
+  // Локальное состояние для управления выбранной датой
+  const [localSelected, setLocalSelected] = useState<DataType>(selected)
+
+  // Используем хук useDebounce, чтобы уменьшить частоту обновлений выбранного значения в форме
+  const debouncedSelected = useDebounce(localSelected, 500)
+
+  // Синхронизируем локальное состояние с пропсами, когда они изменяются
+  useEffect(() => {
+    setLocalSelected(selected)
+  }, [selected])
+
+  // Обновляем значение в форме через дебаунс
+  useEffect(() => {
+    if (debouncedSelected) {
+      onChange(debouncedSelected)
+    }
+  }, [debouncedSelected, onChange])
+
+  // Обработчик изменения выбранной даты, который немедленно обновляет локальное состояние
+  const handleSelect = (date: DataType) => {
+    setLocalSelected(date)
+  }
+
   return (
     <InputDataPicker
-      selected={selected}
-      onSelect={onChange}
+      selected={localSelected} // Мгновенное обновление UI с локальным состоянием
+      onSelect={handleSelect}
       labelInput={label}
       error={error}
       disabled={disabled}
