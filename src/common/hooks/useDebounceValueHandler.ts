9febc5520cf431f75a1c15530945daa0e434e9eb
k2 <izmail.k2@gmail.com>
2024-10-20 15:09:46 +0300

    feat: added hook useDebounceValueHandler to manage state with debounce
    Feat: optimised the date picker to reduce the number of re-renders with debounce and local state management

---
+++ src/common/hooks/useDebounceValueHandler.ts
@@ -0,0 +1,48 @@
+import { useCallback, useRef, useState } from 'react'
+
+// Определяем тип возвращаемого значения хука, который включает
+// локальное значение состояния и функцию для обработки изменений значения
+type UseDebounceValueHandlerResult<T> = {
+  localValue: T
+  handleSelect: (value: T) => void
+}
+// Определяем тип параметров, которые принимает хук
+type UseDebounceValueHandlerParams<T> = {
+  initialValue: T
+  onChange: (value: T) => void
+  delay: number
+}
+// Кастомный хук, который предоставляет дебаунс для обработки обновления значения
+export const useDebounceValueHandler = <T>({
+  initialValue, // Начальное значение для локального состояния
+  onChange, // Колбэк-функция, выполняемая после задержки дебаунса
+  delay, // Задержка дебаунса в миллисекундах
+}: UseDebounceValueHandlerParams<T>): UseDebounceValueHandlerResult<T> => {
+  // Состояние для отслеживания текущего значения
+  const [localValue, setLocalValue] = useState<T>(initialValue)
+
+  // useRef для хранения таймера дебаунса, чтобы можно было его очистить при необходимости
+  const debounceTimer = useRef<number | null>(null)
+
+  // useCallback для мемоизации функции handleSelect, чтобы избежать ненужных пересозданий
+  const handleSelect = useCallback(
+    (value: T) => {
+      // Обновляем локальное состояние немедленно
+      setLocalValue(value)
+
+      // Если таймер уже существует, очищаем его, чтобы избежать выполнения предыдущей отложенной функции
+      if (debounceTimer.current) {
+        clearTimeout(debounceTimer.current)
+      }
+
+      // Устанавливаем новый таймер дебаунса для вызова функции onChange после указанной задержки
+      debounceTimer.current = window.setTimeout(() => {
+        onChange(value)
+      }, delay)
+    },
+    [onChange, delay] // Зависимости для useCallback, чтобы функция обновлялась при их изменении
+  )
+
+  // Возвращаем текущее значение и функцию handleSelect
+  return { localValue, handleSelect }
+}
