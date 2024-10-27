a9d651f92c9ab0ffc76ae9067d73b1488e02bc8a
k2 <izmail.k2@gmail.com>
2024-10-27 21:29:55 +0300

    feat: add PrivateLayout

--- src/common/components/Layout/PrivatLayout.tsx
+++ src/common/components/Layout/PrivatLayout.tsx
@@ -1,14 +1,13 @@
 import React, { PropsWithChildren, ReactElement } from 'react'
 import { BaseLayout, Sidebar } from '@/common/components'
 import { NextPage } from 'next'
-import { cn } from '@/common/utils/cn'
 
 const Layout: NextPage<PropsWithChildren> = ({ children }) => {
   return (
     <BaseLayout>
-      <div className="flex flex-1 max-w-screen-desktop w-full m-auto">
+      <div className="flex flex-1 max-w-screen-desktop w-full">
         <Sidebar />
-        <main className={cn('flex-1 mx-auto w-full')}>{children}</main>
+        {children}
       </div>
     </BaseLayout>
   )
