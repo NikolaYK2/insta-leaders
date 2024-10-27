2de999b7013d319c5748ad83fb71c1b5d6a17dd4
k2 <izmail.k2@gmail.com>
2024-10-27 21:33:49 +0300

    feat: define private components and wrap them with layout

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,5 +1,5 @@
-import { getLayout } from '@/common/components'
 import { Favorites } from '@/features/userHub/ui'
+import { PrivateLayout } from '@/common/components/Layout/PrivatLayout'
 
-Favorites.getLayout = getLayout
+Favorites.getLayout = PrivateLayout
 export default Favorites
