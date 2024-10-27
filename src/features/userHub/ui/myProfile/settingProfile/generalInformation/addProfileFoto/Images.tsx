a86e5ce1257b7ffd1d04f665027870f005ff256c
k2 <izmail.k2@gmail.com>
2024-10-27 17:11:51 +0300

    style: add mobile

--- src/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/Images.tsx
+++ src/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/Images.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect, useRef, useState } from 'react'
+import { ChangeEvent, useRef, useState } from 'react'
 
 export function prepareImageForUpload(dataUrl: string, fieldName: string = 'file'): FormData {
   // Create FormData
