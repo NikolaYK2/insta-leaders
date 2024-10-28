a62832a9f277c8626c17c21adb66ed239c9ea8ba
k2 <izmail.k2@gmail.com>
2024-10-28 16:35:07 +0300

    feat: add private Home

---
+++ src/features/userHub/ui/home/Home.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { NextPageWithLayout } from '@/pages/_app'
+
+export const Home: NextPageWithLayout = () => {
+  return <div>Home</div>
+}
---
+++ src/pages/home/index.ts
@@ -0,0 +1,5 @@
+import { PrivateLayout } from '@/common/components/Layout/PrivatLayout'
+import Home from '@/pages'
+
+Home.getLayout = PrivateLayout
+export default Home
