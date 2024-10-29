567719c3e78fd0b6d4f3340b60f3a10cc050c107
k2 <izmail.k2@gmail.com>
2024-10-29 19:41:52 +0300

    refactor: add renderError

--- src/features/userHub/ui/myProfile/settingProfile/generalInformation/renderAgeError/RenderAgeError.tsx
+++ src/features/userHub/ui/myProfile/settingProfile/generalInformation/renderAgeError/RenderAgeError.tsx
@@ -1,24 +1,26 @@
 import { Typography, TypographyVariant } from '@nikolajk2/lib-insta-leaders'
 import Link from 'next/link'
 import { ROUTES_AUTH } from '@/appRoot/routes/routes'
+import React from 'react'
 
 type Props = {
-  age: number | null
+  age: number
 }
-export const RenderAgeError = ({ age }: Props) => {
-  if (age !== null && age < 13) {
-    return (
-      <div className="flex">
-        <Typography variant={TypographyVariant.small_text}>
-          A user under 13 cannot create a profile.
-        </Typography>
-        <Typography className="text-danger-500 ml-1" variant={TypographyVariant.small_link}>
-          <Link href={{ pathname: ROUTES_AUTH.PRIVACY_POLICY, query: { from: 'profile' } }}>
-            Privacy Policy
-          </Link>
-        </Typography>
-      </div>
-    )
-  }
-  return null
+const RenderAgeError = ({ age }: Props) => {
+  return (
+    <div className="flex">
+      <Typography variant={TypographyVariant.small_text}>
+        A user under {age} cannot create a profile.
+      </Typography>
+      <Typography className="text-danger-500 ml-1" variant={TypographyVariant.small_link}>
+        <Link href={{ pathname: ROUTES_AUTH.PRIVACY_POLICY, query: { from: 'profile' } }}>
+          Privacy Policy
+        </Link>
+      </Typography>
+    </div>
+  )
+}
+
+export const renderError = (age: number | null) => {
+  return age !== null && age < 13 ? <RenderAgeError age={13} /> : null
 }
