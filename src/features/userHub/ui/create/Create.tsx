1a401394ab00d9cbec233104f4d11a1a28ed1bed
k2 <izmail.k2@gmail.com>
2024-10-29 23:25:12 +0300

    feat: add ModalClose - add VisibilityToggle for ModalDescription - add ModalContentItem

--- src/features/userHub/ui/create/Create.tsx
+++ src/features/userHub/ui/create/Create.tsx
@@ -1,13 +1,54 @@
 import React from 'react'
-import { Modal, ModalContent, ModalTitle } from '@nikolajk2/lib-insta-leaders'
-import { DialogProps } from '@radix-ui/react-dialog'
+import {
+  Button,
+  DynamicIcon,
+  Modal,
+  ModalClose,
+  ModalContent,
+  ModalContentItem,
+  ModalDescription,
+  ModalProps,
+  ModalTitle,
+  Typography,
+  TypographyVariant,
+  VisibilityToggle,
+} from '@nikolajk2/lib-insta-leaders'
+import { PhotoPreview } from '@/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/AddProfilePhoto'
 
-export const Create = ({ ...props }: DialogProps) => {
+type Props = ModalProps & {
+  className?: string
+}
+export const Create = ({ className, ...props }: Props) => {
   return (
     <Modal {...props}>
-      <ModalContent>
-        <ModalTitle>Title</ModalTitle>
-        123
+      <ModalContent className={'max-w-[492px]'}>
+        <ModalTitle asChild>
+          <Typography variant={TypographyVariant.h1} asChild>
+            <h2>Add Photo</h2>
+          </Typography>
+        </ModalTitle>
+
+        <ModalClose className={'absolute top-[18px] right-4'}>
+          <DynamicIcon iconId={'Close'} width={28} height={28} />
+        </ModalClose>
+
+        <VisibilityToggle>
+          <ModalDescription>image post</ModalDescription>
+        </VisibilityToggle>
+
+        <ModalContentItem className={'flex flex-col justify-center pt-[70px] pb-12'}>
+          <div className={'flex justify-center'}>
+            <PhotoPreview image={''} size={20} />
+          </div>
+          <div className={'flex flex-col mx-auto'}>
+            <Button className={'mb-6 mt-10'}>
+              <Typography variant={TypographyVariant.h3}>Select from Computer</Typography>
+            </Button>
+            <Button variant={'outline'}>
+              <Typography variant={TypographyVariant.h3}>Open Draft</Typography>
+            </Button>
+          </div>
+        </ModalContentItem>
       </ModalContent>
     </Modal>
   )
