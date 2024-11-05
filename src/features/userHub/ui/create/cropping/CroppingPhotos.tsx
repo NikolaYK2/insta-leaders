bf9ac3bcfe95ff1803956b7b10e609c674498de2
k2 <izmail.k2@gmail.com>
2024-11-05 19:45:16 +0300

    feat: add handleDeletePhoto for photos - add stopPropagation

--- src/features/userHub/ui/create/cropping/CroppingPhotos.tsx
+++ src/features/userHub/ui/create/cropping/CroppingPhotos.tsx
@@ -1,31 +1,48 @@
 import { SelectedImages } from '@/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/Images'
 import React, { ChangeEvent, forwardRef } from 'react'
-import Image from 'next/image'
 import { Button, DynamicIcon } from '@nikolajk2/lib-insta-leaders'
 import { ImageUploader } from '@/common/components/imageUpLoader'
+import { PhotoPreview } from '@/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/AddProfilePhoto'
 
 type CroppingPhotosProps = {
   selectedImages: SelectedImages[]
-  error: string | null
+  setSelectedImages: (selectedImages: SelectedImages[]) => void
   setImageCrop: (image: SelectedImages) => void
   callBack: () => void
   handleFileChange: (event: ChangeEvent<HTMLInputElement>) => void
 }
 const CroppingPhotos = forwardRef<HTMLInputElement, CroppingPhotosProps>(
-  ({ error, selectedImages, setImageCrop, callBack, handleFileChange }, ref) => {
+  ({ selectedImages, setImageCrop, callBack, handleFileChange, setSelectedImages }, ref) => {
+    const handleDeletePhoto = (id: string) => {
+      setSelectedImages(selectedImages.filter(img => img.id !== id))
+      // Теперь setSelectedImages обновит состояние, и картинка будет удалена
+    }
     return (
-      <div className={'absolute flex items-start bottom-[60px] right-3 bg-dark-500/40'}>
-        <span>{error}</span>
+      <div
+        className={'absolute flex items-start bottom-[60px] right-3 bg-dark-500/40'}
+        onClick={e => e.stopPropagation()}
+      >
         <div className={'flex flex-wrap'}>
           {selectedImages.map(img => (
             <div key={img.id} className={'flex max-w-[82px] max-h-[82px] overflow-hidden m-1.5'}>
-              <Image
-                className={'max-w-20 object-contain'}
-                src={img.image}
-                alt={'img'}
-                width={82}
-                height={82}
+              {/*<Image*/}
+              {/*  className={'max-w-20 object-contain'}*/}
+              {/*  src={img.image}*/}
+              {/*  alt={'img'}*/}
+              {/*  width={82}*/}
+              {/*  height={82}*/}
+              {/*  onClick={() => setImageCrop({ image: img.image, id: img.id })}*/}
+              {/*/>*/}
+              {/*<div>X</div>*/}
+              <PhotoPreview
+                image={img.image}
+                size={82}
+                className={'w-full h-full object-cover object-center cursor-pointer'}
+                styleBackground={'max-w-20 max-h-20'}
+                styleImage={'max-w-20 max-h-20 rounded-none'}
+                styleClose={'top-1 right-1 rounded-none bg-dark-100'}
                 onClick={() => setImageCrop({ image: img.image, id: img.id })}
+                callback={() => handleDeletePhoto(img.id)}
               />
             </div>
           ))}
