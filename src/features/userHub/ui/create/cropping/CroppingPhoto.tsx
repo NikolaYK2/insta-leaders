45971932b5dc3cd24df214ab37a3e37002ee9f23
k2 <izmail.k2@gmail.com>
2024-11-07 13:21:37 +0300

    refactor: use selectors & dispatch

--- src/features/userHub/ui/create/cropping/CroppingPhoto.tsx
+++ src/features/userHub/ui/create/cropping/CroppingPhoto.tsx
@@ -1,70 +1,59 @@
 import Cropper, { Area, MediaSize } from 'react-easy-crop'
 import { Button, DynamicIcon } from '@nikolajk2/lib-insta-leaders'
 import { cn } from '@/common/utils/cn'
-import React, { Dispatch, SetStateAction, useCallback, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { getCroppedImg } from '@/common/utils'
-import { SelectedImages } from '@/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/Images'
+import { useAppDispatch, useAppSelector } from '@/appRoot/lib/hooks/hooksStore'
+import {
+  indexCropImageSelector,
+  selectedImagesSelector,
+} from '@/features/userHub/model/createSlice/createSelectors'
+import { actionsCreate } from '@/features/userHub/model/createSlice'
 
 type CarouselBtn = {
-  id: number
+  id: string
   iconId: 'ArrowIosForward'
   classname: string
 }
 const carouselBtn: CarouselBtn[] = [
   {
-    id: 1,
+    id: 'next',
     iconId: 'ArrowIosForward',
     classname: 'left-3 rotate-180',
   },
   {
-    id: 2,
+    id: 'back',
     iconId: 'ArrowIosForward',
     classname: 'right-3',
   },
 ]
 
 type Props = {
-  selectedImages: SelectedImages[]
-  setSelectedImages: (selectedImages: SelectedImages[]) => void
-  indexCropImage: number
-  setIndexCropImage: Dispatch<SetStateAction<number>>
   aspect: number | undefined
   setAspect: (aspect: number | undefined) => void
   setAspectOriginal: (aspect: number | undefined) => void
   zoom: number
   setZoom: (zoom: number) => void
 }
-export const CroppingPhoto = ({
-  selectedImages,
-  aspect,
-  setAspect,
-  setAspectOriginal,
-  setZoom,
-  zoom,
-  indexCropImage,
-  setIndexCropImage,
-  setSelectedImages,
-}: Props) => {
+export const CroppingPhoto = ({ aspect, setAspect, setAspectOriginal, setZoom, zoom }: Props) => {
   const [crop, setCrop] = useState({ x: 0, y: 0 }) //координаты для cropper
   const [lastTap, setLastTap] = useState<number>(0) //обрезка для двойного нажатия на мобилках
   const [croppedArea, setCroppedArea] = useState<Area>() //координаты после обрезки фото
-
+  const images = useAppSelector(selectedImagesSelector)
+  const indexCropImage = useAppSelector(indexCropImageSelector)
+  const dispatch = useAppDispatch()
   const onCropComplete = (croppedAreaPercentage: Area, croppedAreaPixels: Area) => {
     setCroppedArea(croppedAreaPixels)
   }
 
   const handleGenerateCroppedImage = async () => {
-    if (selectedImages.length === 0) return
+    if (images.length === 0) return
 
     try {
-      const url = await getCroppedImg(selectedImages[indexCropImage]?.image ?? '', croppedArea)
+      const url = await getCroppedImg(images[indexCropImage]?.image ?? '', croppedArea)
       if (url) {
         // Обновляем текущее изображение в `selectedImages` на обрезанное
-        setSelectedImages(
-          selectedImages.map((img, index) =>
-            index === indexCropImage ? { ...img, image: url as string } : img
-          )
-        )
+        dispatch(actionsCreate.setCroppedImage({ url }))
       }
     } catch (error) {
       console.error('Error generating cropped image:', error)
@@ -90,29 +79,24 @@ export const CroppingPhoto = ({
     setLastTap(currentTime)
   }
 
-  // // для слайдов
-  const handleGetCarousel = (idBtn: number) => {
-    setIndexCropImage(prevIndex => {
-      if (idBtn === 2 && prevIndex < selectedImages.length - 1) {
-        // Переход к следующему изображению, если не достигнут конец
-        return prevIndex + 1
-      } else if (idBtn === 1 && prevIndex > 0) {
-        // Переход к предыдущему изображению, если не достигнуто начало
-        return prevIndex - 1
-      }
-      return prevIndex
-    })
+  // для слайдов
+  const handleGetCarousel = (idBtn: string) => {
+    const newIndex =
+      idBtn === 'back'
+        ? Math.min(indexCropImage + 1, images.length - 1) // Переход к следующему изображению
+        : Math.max(indexCropImage - 1, 0) // Переход к предыдущему изображению
+
+    dispatch(actionsCreate.setIndexCropImage(newIndex))
   }
 
-  // Переменные для управления видимостью кнопок
   const isPreviousHidden = indexCropImage === 0
-  const isNextHidden = indexCropImage === selectedImages.length - 1
+  const isNextHidden = indexCropImage === images.length - 1
 
   return (
     <>
       <div onDoubleClick={handleGenerateCroppedImage} onTouchEnd={handleTouch}>
         <Cropper
-          image={selectedImages[indexCropImage]?.image ?? ''} // Показываем текущее изображение
+          image={images[indexCropImage]?.image ?? ''} // Показываем текущее изображение
           aspect={aspect}
           crop={crop}
           onCropChange={setCrop}
@@ -129,9 +113,9 @@ export const CroppingPhoto = ({
             key={btn.id}
             className={cn(
               'absolute top-[50%] p-2 bg-dark-500/80 text-light-100',
-              selectedImages.length === 1 && 'hidden',
-              btn.id === 1 && isPreviousHidden && 'hidden',
-              btn.id === 2 && isNextHidden && 'hidden',
+              images.length === 1 && 'hidden',
+              btn.id === 'next' && isPreviousHidden && 'hidden',
+              btn.id === 'back' && isNextHidden && 'hidden',
               btn.classname
             )}
             variant={'text'}
