80663406dac28066b32771e26a05dfb784803dca
k2 <izmail.k2@gmail.com>
2024-11-07 15:25:55 +0300

    refactor: add prop - rename props

--- src/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/AddProfilePhoto.tsx
+++ src/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/AddProfilePhoto.tsx
@@ -4,17 +4,20 @@ import { AddPhotoModal } from './AddPhotoModal'
 import { useAvatar } from './useAvatar'
 
 import { PhotoPreview } from './PhotoPreview'
+import { usePhotoPreview } from '@/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/usePhotoPreview'
 
 export const AddProfilePhoto = () => {
   const { handleDeletePhoto, image, setImage } = useAvatar()
+  const { handleConfirmation } = usePhotoPreview(handleDeletePhoto)
 
   return (
     <div className={'flex flex-col items-center gap-6 w-full max-w-xs pt-6'}>
       <PhotoPreview
         image={image}
         size={96}
-        onDeletePhoto={handleDeletePhoto}
-        styleImage={'h-192 w-192 object-cover rounded-full'}
+        callback={handleConfirmation}
+        styleImage={'object-cover rounded-full'}
+        styleClose={'shadow-[0px_0px_0px_4px_black] right-3'}
       />
       <AddPhotoModal setImage={setImage} />
     </div>
