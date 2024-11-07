11dfec6e393883003e1dbc846dbb3e339583bcc1
k2 <izmail.k2@gmail.com>
2024-11-07 15:23:58 +0300

    refactor: removed

--- src/common/components/ConfirmationModal/ConfirmationModal.tsx
+++ src/common/components/ConfirmationModal/ConfirmationModal.tsx
@@ -29,33 +29,32 @@ export const ConfirmationModal = ({
     <Modal>
       <ModalTrigger
         className={cn(
-          'cursor-pointer absolute top-[19px] right-2.5 flex items-center justify-center w-6 h-6',
+          'cursor-pointer absolute top-[19px] right-2.5 flex items-center justify-center w-4 h-4 bg-danger-500 rounded-full hover:bg-danger-300',
           className
         )}
       >
-        <DynamicIcon
-          className={'border-4 border-black rounded-full bg-danger-500 hover:bg-danger-300'}
-          iconId={iconId}
-          width={24}
-          color="white"
-        />
+        <DynamicIcon iconId={iconId} width={24} color="white" />
       </ModalTrigger>
       <ModalContent className={'w-full max-w-[492px]'}>
-        <ModalTitle className={'text-light-100'}>
+        <ModalTitle className={'text-light-100'} asChild>
           <Typography variant={TypographyVariant.h2}>Delete Photo</Typography>
         </ModalTitle>
 
-        <ModalContentItem className={'flex flex-col gap-6 items-center w-full p-6 pb-24'}>
-          <ModalDescription>
+        <ModalContentItem
+          className={'flex flex-col max-h-[180px] h-screen justify-between w-full pt-[30px] pb-9'}
+        >
+          <ModalDescription asChild>
             <Typography className={'text-light-100'} variant={TypographyVariant.regular_text_16}>
               Are you sure you want to delete the photo?
             </Typography>
           </ModalDescription>
-          <div>
-            <ModalClose>
-              <Button variant={'outline'}>No</Button>
+          <div className={'flex justify-between max-w-[216px] w-full ml-auto'}>
+            <ModalClose asChild>
+              <Button className={'px-[34px]'} variant={'outline'}>
+                No
+              </Button>
             </ModalClose>
-            <Button onClick={confirmation} variant={'primary'}>
+            <Button className={'px-[34px]'} onClick={confirmation} variant={'primary'}>
               Yes
             </Button>
           </div>
