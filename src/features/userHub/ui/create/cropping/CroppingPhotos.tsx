4fd4365c42c5b135692c95da58ac34961b24b8af
k2 <izmail.k2@gmail.com>
2024-11-07 13:20:36 +0300

    feat: use useModalAddPhoto

--- src/features/userHub/ui/create/cropping/CroppingPhotos.tsx
+++ src/features/userHub/ui/create/cropping/CroppingPhotos.tsx
@@ -1,60 +1,67 @@
-import { SelectedImages } from '@/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/Images'
-import React, { ChangeEvent, forwardRef } from 'react'
+import React from 'react'
 import { Button, DynamicIcon } from '@nikolajk2/lib-insta-leaders'
 import { ImageUploader } from '@/common/components/imageUpLoader'
 import { PhotoPreview } from '@/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/AddProfilePhoto'
+import { useAppDispatch, useAppSelector } from '@/appRoot/lib/hooks/hooksStore'
+import { actionsCreate } from '@/features/userHub/model/createSlice'
+import { selectedImagesSelector } from '@/features/userHub/model/createSlice/createSelectors'
+import { useModalAddPhoto } from '@/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/Images'
 
-type CroppingPhotosProps = {
-  selectedImages: SelectedImages[]
-  setSelectedImages: (selectedImages: SelectedImages[]) => void
-  setIndexCropImage: (index: number) => void
-  callBack: () => void
-  handleFileChange: (event: ChangeEvent<HTMLInputElement>) => void
-}
-const CroppingPhotos = forwardRef<HTMLInputElement, CroppingPhotosProps>(
-  ({ selectedImages, callBack, handleFileChange, setSelectedImages, setIndexCropImage }, ref) => {
-    const handleDeletePhoto = (id: string) => {
-      setSelectedImages(selectedImages.filter(img => img.id !== id))
-      // Теперь setSelectedImages обновит состояние, и картинка будет удалена
-    }
+/**
+ * Здесь будут находиться все фото которые пользователь добавит перед тем как постить
+ */
+const CroppingPhotos = () => {
+  const images = useAppSelector(selectedImagesSelector)
+  const { handleFileChange, handleClick, fileInputRef } = useModalAddPhoto({
+    setActionForImages: actionsCreate.setSelectedImages,
+    photosLength: images.length,
+    photoLimit: 3,
+    errorMessage: 'maximum 10 photos!',
+  })
+  const dispatch = useAppDispatch()
 
-    //выбираем в кроппер картинку при клике
-    const handleClickImage = (id: string) => {
-      const newIndex = selectedImages.findIndex(selectImg => selectImg.id === id) // Находим индекс выбранного изображения
-      if (newIndex !== -1) {
-        setIndexCropImage(newIndex) // Устанавливаем currentIndex на выбранное изображение
-      }
-    }
-    return (
-      <div
-        className={'absolute flex items-start bottom-[60px] right-3 bg-dark-500/40'}
-        onClick={e => e.stopPropagation()}
-      >
-        <div className={'flex flex-wrap'}>
-          {selectedImages.map(img => (
-            <div key={img.id} className={'flex max-w-[82px] max-h-[82px] overflow-hidden m-1.5'}>
-              <PhotoPreview
-                image={img.image}
-                size={82}
-                className={'w-full h-full object-cover object-center cursor-pointer'}
-                styleBackground={'max-w-20 max-h-20'}
-                styleImage={'max-w-20 max-h-20 rounded-none'}
-                styleClose={'top-1 right-1 rounded-none bg-dark-100'}
-                onClick={() => handleClickImage(img.id)}
-                callback={() => handleDeletePhoto(img.id)}
-              />
-            </div>
-          ))}
-        </div>
-        {/*BUTTON ADD PHOTO*/}
-        <Button className={'p-0 m-1.5 text-light-100'} variant={'text'} onClick={callBack}>
-          <DynamicIcon iconId={'PlusCircleOutline'} width={30} height={30} />
-          <ImageUploader handleFileChange={handleFileChange} ref={ref} />
-        </Button>
-      </div>
-    )
+  const handleDeletePhoto = (id: string) => {
+    // Теперь setSelectedImages обновит состояние, и картинка будет удалена
+    dispatch(actionsCreate.deleteImage({ id }))
   }
-)
+
+  //выбираем в кроппер картинку при клике
+  const handleClickImage = (id: string) => {
+    const newIndex = images.findIndex(selectImg => selectImg.id === id) // Находим индекс выбранного изображения
+    if (newIndex !== -1) {
+      dispatch(actionsCreate.setIndexCropImage(newIndex)) // Устанавливаем currentIndex на выбранное изображение
+    }
+  }
+
+  return (
+    <div
+      className={'absolute flex items-start bottom-[60px] right-3 bg-dark-500/40'}
+      onClick={e => e.stopPropagation()}
+    >
+      <div className={'flex flex-wrap'}>
+        {images.map(img => (
+          <div key={img.id} className={'flex max-w-[82px] max-h-[82px] overflow-hidden m-1.5'}>
+            <PhotoPreview
+              image={img.image}
+              size={82}
+              className={'w-full h-full object-cover object-center cursor-pointer'}
+              styleBackground={'max-w-20 max-h-20'}
+              styleImage={'max-w-20 max-h-20 rounded-none'}
+              styleClose={'top-1 right-1 rounded-none bg-dark-100'}
+              onClick={() => handleClickImage(img.id)}
+              callback={() => handleDeletePhoto(img.id)}
+            />
+          </div>
+        ))}
+      </div>
+      {/*BUTTON ADD PHOTOS*/}
+      <Button className={'p-0 m-1.5 text-light-100'} variant={'text'} onClick={handleClick}>
+        <DynamicIcon iconId={'PlusCircleOutline'} width={30} height={30} />
+        <ImageUploader handleFileChange={handleFileChange} ref={fileInputRef} />
+      </Button>
+    </div>
+  )
+}
 CroppingPhotos.displayName = 'PostsPhotos'
 
 export const MemoizedCroppingPhotos = React.memo(CroppingPhotos)
