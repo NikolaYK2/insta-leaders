420daec47e625f98625acda1f3f8c2f661514853
k2 <izmail.k2@gmail.com>
2024-11-07 13:38:20 +0300

    refactor: remove props

--- src/features/userHub/ui/create/addPhoto/AddPhoto.tsx
+++ src/features/userHub/ui/create/addPhoto/AddPhoto.tsx
@@ -1,38 +1,37 @@
-import React, { ChangeEvent, forwardRef } from 'react'
+import React from 'react'
 import { PhotoPreview } from '@/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/AddProfilePhoto'
 import { Button, Typography, TypographyVariant } from '@nikolajk2/lib-insta-leaders'
 import { ImageUploader } from '@/common/components/imageUpLoader'
 import { ErrorMessage } from '@/common/components/ErrorMessage/ErrorMessage'
+import { useModalAddPhoto } from '@/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/Images'
+import { actionsCreate } from '@/features/userHub/model/createSlice'
 
-type PropsAddPhoto = {
-  handleFileChange?: (event: ChangeEvent<HTMLInputElement>) => void
-  handleCLick: () => void
-  image: string | null
-  error: string | null
+export const AddPhoto = () => {
+  const { handleFileChange, handleClick, fileInputRef, error } = useModalAddPhoto({
+    setActionForImages: actionsCreate.setSelectedImages,
+  })
+
+  return (
+    <>
+      <div className={'flex flex-col justify-between items-center h-full'}>
+        {error && <ErrorMessage className={'absolute'}>{error}</ErrorMessage>}
+        <PhotoPreview
+          styleImage={'rounded-none w-[222px] h-[222px] mb-[60px] mt-[72px]'}
+          image={''}
+          size={20}
+        />
+      </div>
+      <div className={'flex flex-col mx-auto'}>
+        <Button className={'mb-6'} onClick={handleClick}>
+          Select from Computer
+          <ImageUploader handleFileChange={handleFileChange} ref={fileInputRef} />
+        </Button>
+
+        <Button variant={'outline'}>
+          <Typography variant={TypographyVariant.h3}>Open Draft</Typography>
+        </Button>
+      </div>
+    </>
+  )
 }
-export const AddPhoto = forwardRef<HTMLInputElement, PropsAddPhoto>(
-  ({ handleFileChange, handleCLick, image, error }, ref) => {
-    return (
-      <>
-        <div className={'flex flex-col justify-between items-center h-full'}>
-          {error && <ErrorMessage className={'absolute'}>{error}</ErrorMessage>}
-          <PhotoPreview
-            styleImage={'rounded-none w-[222px] h-[222px] mb-[60px] mt-[72px]'}
-            image={image}
-            size={20}
-          />
-        </div>
-        <div className={'flex flex-col mx-auto'}>
-          <Button className={'mb-6'} onClick={handleCLick}>
-            Select from Computer
-            <ImageUploader handleFileChange={handleFileChange} ref={ref} />
-          </Button>
-          <Button variant={'outline'}>
-            <Typography variant={TypographyVariant.h3}>Open Draft</Typography>
-          </Button>
-        </div>
-      </>
-    )
-  }
-)
 AddPhoto.displayName = 'AddPhoto'
