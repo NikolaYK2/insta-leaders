256ddf512debef571cc7bd2f6a9b5261daf7811c
k2 <izmail.k2@gmail.com>
2024-11-07 13:37:23 +0300

    refactor: add selectors

--- src/features/userHub/ui/create/Create.tsx
+++ src/features/userHub/ui/create/Create.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Button,
   DynamicIcon,
@@ -17,43 +17,36 @@ import { cn } from '@/common/utils/cn'
 import { Cropping } from '@/features/userHub/ui/create/cropping/Cropping'
 import { useModalAddPhoto } from '@/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/Images'
 import { AddPhoto } from '@/features/userHub/ui/create/addPhoto'
+import { useAppSelector } from '@/appRoot/lib/hooks/hooksStore'
+import { selectedImagesSelector } from '@/features/userHub/model/createSlice/createSelectors'
+import { ConfirmationModal } from '@/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/modalAddPhoto'
 
 type Props = ModalProps & {
   className?: string
 }
 export const Create = ({ className, open, onOpenChange, ...props }: Props) => {
-  const {
-    handleFileChange,
-    selectedImage,
-    selectedImages,
-    setSelectedImages,
-    handleClick,
-    fileInputRef,
-    reset,
-    error,
-  } = useModalAddPhoto({
-    isOpen: true,
-    setImage: () => {},
-  })
+  const images = useAppSelector(selectedImagesSelector)
+  const image = images.length
+  const { reset } = useModalAddPhoto({})
 
   return (
     <Modal {...props} open={open} onOpenChange={onOpenChange}>
       <ModalContent className={'flex flex-col max-w-[492px] h-[564px]'}>
-        <ModalTitle className={cn('flex', selectedImage && 'justify-center')} asChild>
+        <ModalTitle className={cn('flex', image && 'justify-center')} asChild>
           <Typography variant={TypographyVariant.h1} asChild>
-            <h2>{selectedImage ? 'Cropping' : 'Add Photo'}</h2>
+            <h2>{image ? 'Cropping' : 'Add Photo'}</h2>
           </Typography>
         </ModalTitle>
 
-        {selectedImage ? (
+        {image ? (
           <div>
-            {/*кнопка назад*/}
-            <Button
-              className="absolute top-[16px] left-3.5 p-0"
-              variant={'secondary'}
-              onClick={reset}
-            >
-              <DynamicIcon iconId={'ArrowIosBack'} width={28} height={28} />
+            {/*BUTTON BACK*/}
+            <Button className="absolute top-[16px] left-3.5 p-0" variant={'secondary'}>
+              <ConfirmationModal
+                className={'relative block inset-0 w-[30px] bg-transparent h-full rounded-none'}
+                iconId={'ArrowIosBack'}
+                confirmation={reset}
+              />
             </Button>
 
             <Button className={'absolute top-[11px] right-1'} variant={'text'}>
@@ -76,29 +69,9 @@ export const Create = ({ className, open, onOpenChange, ...props }: Props) => {
         </VisibilityToggle>
 
         <ModalContentItem
-          className={cn(
-            'relative flex flex-col pt-[0px] pb-12 h-full z-50',
-            selectedImage && 'p-3'
-          )}
+          className={cn('relative flex flex-col pt-[0px] pb-12 h-full z-50', image && 'p-3')}
         >
-          {selectedImage ? (
-            <Cropping
-              callBack={handleClick}
-              selectedImages={selectedImages}
-              setSelectedImages={setSelectedImages}
-              ref={fileInputRef}
-              handleFileChange={handleFileChange}
-              error={error}
-            />
-          ) : (
-            <AddPhoto
-              handleFileChange={handleFileChange}
-              handleCLick={handleClick}
-              ref={fileInputRef}
-              image={selectedImage}
-              error={error}
-            />
-          )}
+          {image ? <Cropping /> : <AddPhoto />}
         </ModalContentItem>
       </ModalContent>
     </Modal>
