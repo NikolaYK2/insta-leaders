fc740cb1f401105163dff280fac65525135667d0
k2 <izmail.k2@gmail.com>
2024-11-13 19:39:22 +0300

    refactor: rename selectors

--- src/features/userHub/model/createSlice/createSelectors.ts
+++ src/features/userHub/model/createSlice/createSelectors.ts
@@ -1,9 +1,9 @@
 import { AppState } from '@/appRoot/store'
 import { SelectedImages } from '@/features/userHub/model/createSlice/createSlice'
 
-export const selectedImagesSelector = (state: AppState): SelectedImages[] =>
+export const selectorSelectedImages = (state: AppState): SelectedImages[] =>
   state.create.selectedImages
 
-export const indexCropImageSelector = (state: AppState) => state.create.indexCropImage
+export const selectorIndexCropImage = (state: AppState) => state.create.indexCropImage
 
 export const errorSelector = (state: AppState) => state.create.error
