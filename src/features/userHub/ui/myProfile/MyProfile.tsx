90acf46fb2f6cbb32edbcd2c2a9bca4e16aedafa
k2 <izmail.k2@gmail.com>
2024-11-28 17:12:03 +0300

    refactor: temp comment

--- src/features/userHub/ui/myProfile/MyProfile.tsx
+++ src/features/userHub/ui/myProfile/MyProfile.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { NextPageWithLayout } from '@/pages/_app'
 import { Page } from '@/common/components/page'
-import { Button, Typography, TypographyVariant } from '@nikolajk2/lib-insta-leaders'
+import { Typography, TypographyVariant } from '@nikolajk2/lib-insta-leaders'
 import { ROUTES_APP } from '@/appRoot/routes/routes'
 import { useRouter } from 'next/router'
-import { useGetUsersMeQuery, useGetUsersPostsQuery } from '@/features/userHub/api/user/userService'
+import { useGetUsersPostsQuery } from '@/features/userHub/api/user/userService'
 import Image from 'next/image'
-import { AddPhotoPreview } from '@/features/userHub/ui/myProfile/settingProfile/generalInformation/addProfileFoto/AddPhotoModal'
 
 const testPosts = [
   //временные данные для теста
@@ -34,16 +33,16 @@ const testPosts = [
 
 export const MyProfile: NextPageWithLayout = () => {
   const router = useRouter()
-  const { data: userMe, isLoading: isLoadingUserMe, isError: isErrorUserMe } = useGetUsersMeQuery()
+  // const { data: userMe, isLoading: isLoadingUserMe, isError: isErrorUserMe } = useGetUsersMeQuery()
   const {
     data: userPosts,
     isLoading: isLoadingUserPosts,
     isError: isErrorUserPosts,
   } = useGetUsersPostsQuery()
 
-  if (isLoadingUserMe) {
-    return <div>Loading...</div>
-  }
+  // if (isLoadingUserMe) {
+  //   return <div>Loading...</div>
+  // }
 
   const handlerClickRedirectSetting = () => {
     router.push(`${ROUTES_APP.PROFILE}${ROUTES_APP.PROFILE_SETTING}`)
@@ -61,25 +60,25 @@ export const MyProfile: NextPageWithLayout = () => {
     >
       <section className={'flex justify-between flex-wrap mb-12'}>
         <div className={'max-w-[204px] h-[204px] rounded-full'}>
-          <AddPhotoPreview
-            image={userMe?.data.avatar ?? null}
-            size={192}
-            containerClassName={
-              'relative overflow-hidden flex items-center justify-center w-[204px] h-[204px] rounded-full m-0 p-0 bg-dark-500'
-            }
-          />
+          {/*<AddPhotoPreview*/}
+          {/*  image={userMe?.data.avatar ?? null}*/}
+          {/*  size={192}*/}
+          {/*  containerClassName={*/}
+          {/*    'relative overflow-hidden flex items-center justify-center w-[204px] h-[204px] rounded-full m-0 p-0 bg-dark-500'*/}
+          {/*  }*/}
+          {/*/>*/}
         </div>
         <div className={'w-full max-w-[730px]'}>
-          <div className={'flex justify-between items-center mb-5'}>
-            <Typography>{userMe?.data.userName ?? 'User name'}</Typography>
-            {userMe?.data.id &&
-              router.query.id &&
-              userMe?.data.id === profileId && ( //являешься ли владельцем профиля
-                <Button variant={'secondary'} onClick={handlerClickRedirectSetting}>
-                  <Typography variant={TypographyVariant.h3}>Profile Settings</Typography>
-                </Button>
-              )}
-          </div>
+          {/*<div className={'flex justify-between items-center mb-5'}>*/}
+          {/*  <Typography>{userMe?.data.userName ?? 'User name'}</Typography>*/}
+          {/*  {userMe?.data.id &&*/}
+          {/*    router.query.id &&*/}
+          {/*    userMe?.data.id === profileId && ( //являешься ли владельцем профиля*/}
+          {/*      <Button variant={'secondary'} onClick={handlerClickRedirectSetting}>*/}
+          {/*        <Typography variant={TypographyVariant.h3}>Profile Settings</Typography>*/}
+          {/*      </Button>*/}
+          {/*    )}*/}
+          {/*</div>*/}
 
           <div className={'flex flex-row mb-7'}>
             <div className={'max-w-[159px] w-full mr-1'}>
@@ -96,22 +95,22 @@ export const MyProfile: NextPageWithLayout = () => {
             </div>
           </div>
 
-          <Typography variant={TypographyVariant.regular_text_16} className={''}>
-            {userMe?.data.aboutMe ? (
-              <>
-                {userMe?.data.aboutMe}
-                <Typography
-                  asChild
-                  variant={TypographyVariant.regular_link}
-                  className={'cursor-pointer'}
-                >
-                  <span>span span span</span>
-                </Typography>
-              </>
-            ) : (
-              '...'
-            )}
-          </Typography>
+          {/*<Typography variant={TypographyVariant.regular_text_16} className={''}>*/}
+          {/*  {userMe?.data.aboutMe ? (*/}
+          {/*    <>*/}
+          {/*      {userMe?.data.aboutMe}*/}
+          {/*      <Typography*/}
+          {/*        asChild*/}
+          {/*        variant={TypographyVariant.regular_link}*/}
+          {/*        className={'cursor-pointer'}*/}
+          {/*      >*/}
+          {/*        <span>span span span</span>*/}
+          {/*      </Typography>*/}
+          {/*    </>*/}
+          {/*  ) : (*/}
+          {/*    '...'*/}
+          {/*  )}*/}
+          {/*</Typography>*/}
         </div>
       </section>
       <section className={'flex flex-wrap m-[-8px]'}>
