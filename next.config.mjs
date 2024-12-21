667e7f53ebb076befcad00a58b115f1d655e42e5
k2 <izmail.k2@gmail.com>
2024-12-21 16:04:09 +0300

    refactor: remove unnecessary redirects

--- next.config.mjs
+++ next.config.mjs
@@ -12,31 +12,31 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: ['staging-it-incubator.s3.eu-central-1.amazonaws.com'], // 11
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'sociable-people.storage.yandexcloud.net',
-                pathname: '/**'//** - означает что будет какая угодна картинка
-            },
-            {
-                protocol: 'https',
-                hostname: 'storage.yandexcloud.net',
-                pathname: '/**'//** - означает что будет какая угодна картинка
-            }
-        ]
+    //     remotePatterns: [
+    //         {
+    //             protocol: 'https',
+    //             hostname: 'sociable-people.storage.yandexcloud.net',
+    //             pathname: '/**'//** - означает что будет какая угодна картинка
+    //         },
+    //         {
+    //             protocol: 'https',
+    //             hostname: 'storage.yandexcloud.net',
+    //             pathname: '/**'//** - означает что будет какая угодна картинка
+    //         }
+    //     ]
     },
     async redirects() {
         return [
-            {
-                source: '/api/v1/auth/email-confirmation', // длинный путь, который ты хочешь перенаправить
-                destination: '/emailConfirmation', // короткий путь
-                permanent: true, // false означает временный редирект
-            },
-            {
-                source: '/api/v1/auth/password-recovery', // длинный путь, который ты хочешь перенаправить
-                destination: '/createNewPassword', // короткий путь
-                permanent: true, // false означает временный редирект
-            },
+            // {
+            //     source: '/api/v1/auth/email-confirmation', // длинный путь, который ты хочешь перенаправить
+            //     destination: '/emailConfirmation', // короткий путь
+            //     permanent: true, // false означает временный редирект
+            // },
+            // {
+            //     source: '/api/v1/auth/password-recovery', // длинный путь, который ты хочешь перенаправить
+            //     destination: '/createNewPassword', // короткий путь
+            //     permanent: true, // false означает временный редирект
+            // },
             {
                 source: '/',
                 destination: '/signIn',
