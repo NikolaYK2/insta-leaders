bb164523a5c0eb4445197bf19541a8f1f8ba0352
k2 <izmail.k2@gmail.com>
2024-12-22 21:55:57 +0300

    feat: add redirect registration-confirmation

--- next.config.mjs
+++ next.config.mjs
@@ -27,11 +27,11 @@ const nextConfig = {
     },
     async redirects() {
         return [
-            // {
-            //     source: '/api/v1/auth/email-confirmation', // длинный путь, который ты хочешь перенаправить
-            //     destination: '/emailConfirmation', // короткий путь
-            //     permanent: true, // false означает временный редирект
-            // },
+            {
+                source: '/auth/registration-confirmation', // длинный путь, который ты хочешь перенаправить
+                destination: '/emailConfirmation', // короткий путь
+                permanent: true, // false означает временный редирект
+            },
             // {
             //     source: '/api/v1/auth/password-recovery', // длинный путь, который ты хочешь перенаправить
             //     destination: '/createNewPassword', // короткий путь
