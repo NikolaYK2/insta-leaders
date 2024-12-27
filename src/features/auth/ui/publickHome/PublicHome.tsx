34c7ce0f7e731c0d1dd414e1828a3fc3a96dcfd7
k2 <izmail.k2@gmail.com>
2024-12-27 15:29:13 +0300

    feat: use CounterSlot

--- src/features/auth/ui/publickHome/PublicHome.tsx
+++ src/features/auth/ui/publickHome/PublicHome.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import {NextPageWithLayout} from "@/pages/_app";
+import React from 'react'
+import { NextPageWithLayout } from '@/pages/_app'
+import { CounterSlot } from '@/common/components/counterSlot'
+import { Page } from '@/common/components/page'
 
 export const PublicHome: NextPageWithLayout = () => {
   return (
-    <div>
-      Public Home
-    </div>
-  );
-};
+    <Page titleMeta={'Home public'} descriptionMeta={'Explore the public section of our platform.'}>
+      <CounterSlot />
+    </Page>
+  )
+}
