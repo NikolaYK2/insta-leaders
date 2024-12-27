f35bed85b519eb1acaef2c13c29a5f645bf098fb
k2 <izmail.k2@gmail.com>
2024-12-28 00:04:15 +0300

    feat: add timeAgo util

---
+++ src/common/utils/timeAge.ts
@@ -0,0 +1,18 @@
+export const timeAgo = (timestamp: string): string => {
+  const now = new Date();
+  const time = new Date(timestamp);
+  const diffInSeconds = Math.floor((now.getTime() - time.getTime()) / 1000);
+
+  const minutes = Math.floor(diffInSeconds / 60);
+  const hours = Math.floor(diffInSeconds / 3600);
+  const days = Math.floor(diffInSeconds / 86400);
+
+  if (minutes < 1) return "just now";
+  if (minutes < 60) return `${minutes} min ago`;
+  if (hours < 24) return `${hours} hour${hours > 1 ? "s" : ""} ago`;
+  return `${days} day${days > 1 ? "s" : ""} ago`;
+};
+
+// // Пример использования
+// const timestamp = "2024-12-27T10:55:39.619Z";
+// console.log(timeAgo(timestamp));
