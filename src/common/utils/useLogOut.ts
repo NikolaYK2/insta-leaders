8ea399b8cf0b276e724d64c78c0f35edceec1e2d
k2 <izmail.k2@gmail.com>
2024-12-30 18:28:54 +0300

    feat: add redirect

--- src/common/utils/useLogOut.ts
+++ src/common/utils/useLogOut.ts
@@ -2,6 +2,7 @@ import { useLogOutMutation } from "@/features/auth/api/authService";
 import { useRouter } from "next/router";
 import { LocalStorageUtil } from "@/common/utils/LocalStorageUtil";
 import { indexDBUtils } from "@/common/utils/indexedDB";
+import { ROUTES_AUTH } from "@/appRoot/routes/routes";
 
 export const useLogOut = () => {
   const router = useRouter();
@@ -15,6 +16,7 @@ export const useLogOut = () => {
       LocalStorageUtil.removeItem("userId");
       LocalStorageUtil.removeItem("email");
       await indexDBUtils.clearAllImages();
+      await router.push(ROUTES_AUTH.HOME);
     } catch (error) {
       console.error("Ошибка при выходе:", error);
     }
