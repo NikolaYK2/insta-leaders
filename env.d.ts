6fdac78632fbd5732b423da5b9172b36dd4c123b
k2 <izmail.k2@gmail.com>
2025-01-01 21:55:01 +0300

    fix: api

--- env.d.ts
+++ env.d.ts
@@ -3,7 +3,7 @@ declare namespace NodeJS {
     NEXT_PUBLIC_GOOGLE_CLIENT_ID: string;
     NEXT_PUBLIC_GITHUB_CLIENT_ID: string;
     NEXT_PUBLIC_BASE_URL: string;
-    NEXT_PUBLIC_API_URL: string;
+    NEXT_PUBLIC_MAIL_URL: string;
     NEXT_PUBLIC_RECAPTCHA_ENTERPRISE_API_KEY: string;
   }
 }
